a5236a6c9f572611b3f0a0a4e58356637cbca44e
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-06-10 18:43:33 +0530

    Time: 3 ms (96.83%), Space: 7.4 MB (42.89%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -5,20 +5,17 @@ public:
         std::ios_base::sync_with_stdio(false);
         std::cout.tie(nullptr);
         std::cin.tie(nullptr);
-        int k=s1.size(), freq[27], unMatched=k;
-        memset(freq,0,sizeof freq);
+        int k=s1.size(), freq[26]={}, unMatched=k;
+        //memset(freq,0,sizeof freq);
         for(char ch:s1) ++freq[ch-'a'];
         int start=0,end=0;
         for(;end<k;++end){
-            if(freq[s2[end]-'a']>0) --unMatched;
-            --freq[s2[end]-'a'];
+            if(freq[s2[end]-'a']-- >0) --unMatched;
         }
         if(unMatched==0) return true;
         for(;end<s2.size();++end){
-            if(freq[s2[start]-'a']>=0) ++unMatched;
-            ++freq[s2[start++]-'a'];
-            if(freq[s2[end]-'a']>0) --unMatched;
-            --freq[s2[end]-'a'];
+            if(freq[s2[start++]-'a']++ >=0) ++unMatched;
+            if(freq[s2[end]-'a']-- >0) --unMatched;
             if(unMatched==0) return true;
         }
         return false;
