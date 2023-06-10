1b1231a77650c57138a9ce015b66ab5925c188e6
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-06-10 18:28:37 +0530

    Time: 0 ms (100%), Space: 7.2 MB (68.47%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if(s2.size()<s1.size()) return false;
+        std::ios_base::sync_with_stdio(false);
+        std::cout.tie(nullptr);
+        std::cin.tie(nullptr);
+        int k=s1.size(), freq[27], unMatched=k;
+        memset(freq,0,sizeof freq);
+        for(char ch:s1) ++freq[ch-'a'];
+        int start=0,end=0;
+        for(;end<k;++end){
+            if(freq[s2[end]-'a']>0) --unMatched;
+            --freq[s2[end]-'a'];
+        }
+        if(unMatched==0) return true;
+        for(;end<s2.size();++end){
+            if(freq[s2[start]-'a']>=0) ++unMatched;
+            ++freq[s2[start++]-'a'];
+            if(freq[s2[end]-'a']>0) --unMatched;
+            --freq[s2[end]-'a'];
+            if(unMatched==0) return true;
+        }
+        return false;
+    }
+};
