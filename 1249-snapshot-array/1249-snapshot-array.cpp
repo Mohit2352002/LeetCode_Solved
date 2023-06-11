d74edba8dc63710af594f00ed2bcff38e08c00de
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-06-11 15:28:10 +0530

    Time: 424 ms (36.18%), Space: 143.5 MB (74.1%) - LeetHub

---
+++ 1249-snapshot-array/1249-snapshot-array.cpp
@@ -0,0 +1,28 @@
+class SnapshotArray {
+    int count=0;
+    unordered_map<int,map<int,int>>mp;
+public:
+    SnapshotArray(int length) {
+    }
+    
+    void set(int index, int val) {
+        mp[index][count]=val;
+    }
+    
+    int snap() {
+        return count++;
+    }
+    
+    int get(int index, int snap_id) {
+  auto it = mp[index].upper_bound(snap_id);
+  return it == begin(mp[index]) ? 0 : prev(it)->second;
+    }
+};
+
+/**
+ * Your SnapshotArray object will be instantiated and called as such:
+ * SnapshotArray* obj = new SnapshotArray(length);
+ * obj->set(index,val);
+ * int param_2 = obj->snap();
+ * int param_3 = obj->get(index,snap_id);
+ */
