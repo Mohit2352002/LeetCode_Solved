b43c1e3e053c3c79bdd96f5337366a017ff4a93c
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-06-12 13:18:07 +0530

    Time: 0 ms (100%), Space: 11.5 MB (78.97%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,12 +1,19 @@
 class Solution {
-    void helper(vector<string>&ans, int &n, int open, int close, string curr){
+    void helper(vector<string>&ans, int &n, int open, int close, string &curr){
         if(curr.size()==2*n){
             ans.push_back(curr);
             return;
         }
-        if(open<n) helper(ans,n,open+1,close,curr+"("); //can insert opening bracket
-        if(close<open) helper(ans,n,open,close+1,curr+")"); //can insert closing bracket
-        return;
+        if(open<n){
+            curr+="(";
+            helper(ans,n,open+1,close,curr); //can insert opening bracket
+            curr.pop_back();
+        }
+        if(close<open){ 
+            curr+=")";
+            helper(ans,n,open,close+1,curr); //can insert closing bracket
+            curr.pop_back();
+        }
     }
 public:
     vector<string> generateParenthesis(int n) {
@@ -17,7 +24,8 @@ public:
         if(n==2) return {"(())","()()"};
         if(n==3) return {"((()))","(()())","(())()","()(())","()()()"};
         vector<string>ans;
-        helper(ans,n,0,0,"");
+        string curr="";
+        helper(ans,n,0,0,curr);
         return ans;
     }
 };
