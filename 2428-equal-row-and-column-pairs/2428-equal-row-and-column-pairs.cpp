2621b272cf292cf3de3104e4b099634b2ae56b80
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-06-13 09:54:40 +0530

    Time: 77 ms (99.36%), Space: 25.7 MB (81.17%) - LeetHub

--- 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.cpp
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.cpp
@@ -15,13 +15,13 @@ public:
         int n = grid.size();
 
         map<vector<int>, int> row2cnt;
-        for(int i=0;i<n;i++)
-            row2cnt[ grid[i] ]++;
+        for(int i=0;i<n;++i)
+            ++row2cnt[grid[i]];
         
         int cnt = 0;
         vector<int> col(n);
-        for(int j=0;j<n;j++){
-            for(int i=0;i<n;i++)
+        for(int j=0;j<n;++j){
+            for(int i=0;i<n;++i)
                 col[i] = grid[i][j];
             
             //NOTE: inserts (key, 0) if not exist
