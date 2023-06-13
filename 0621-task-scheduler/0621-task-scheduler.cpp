6cb6f429957129f4be9aedf1116c93530b704dde
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-06-13 16:14:27 +0530

    Time: 56 ms (98.27%), Space: 34.3 MB (99.39%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
@@ -5,6 +6,28 @@ public:
         cin.tie(nullptr);
         cout.tie(nullptr);
         if(tasks.size()==1) return 1;
+        if(n==0) return tasks.size();
+
+    }
+};
+*/
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        int t_sz=tasks.size();
+        if(t_sz==1) return 1;
         if(n==0) return tasks.size();
         int freq[26]={}, maxf=0;
         for(char c:tasks){
@@ -14,7 +37,8 @@ public:
         int gaps=(maxf-1)*(n+1);
         for(int el:freq){
             gaps-=min(maxf-1,el);
+            if(gaps<=0) return t_sz;
         }
-        return tasks.size()+max(0,gaps);
+        return t_sz+max(0,gaps);
     }
 };
