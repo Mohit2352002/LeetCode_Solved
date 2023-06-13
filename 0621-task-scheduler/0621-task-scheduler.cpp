b2988c44982eccdfeb56fa194f02c6b5e1098a9c
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-06-13 16:31:04 +0530

    Time: 119 ms (40.26%), Space: 43.8 MB (29.31%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,44 +1,86 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+
+        if(tasks.size()==1) return 1;
+        if(n==0) return tasks.size();
+        
+        priority_queue<int> pq;
+        vector<int>mp(26,0);
+
+        for(char i:tasks){
+            mp[i-'A']++;  // count the number of times a task needs to be done
+        }   
+        for(int i=0;i<26;++i){
+            if(mp[i]) 
+            pq.push(mp[i]);
+        }
+
+        int time=0; // stores the total time taken 
+        while(!pq.empty()){
+            vector<int>remain;
+            int cycle=n+1;  // n+1 is the CPU cycle length, if n is the cooldown period then after a task A there will be n more tasks. Hence n+1.
+
+            while(cycle and !pq.empty()){
+                int max_freq=pq.top(); // the task at the top should be first assigned the CPU as it has highest frequency
+                pq.pop();
+                if(max_freq>1){ // task with more than one occurrence, the next occurrence will be done in the next cycle 
+                    remain.push_back(max_freq-1); // add it to remaining task list
+                }
+                ++time; 
+                --cycle; 
+            }
+
+            for(int count:remain){
+                pq.push(count); 
+            }
+            if(pq.empty())break; // if the priority queue is empty than all tasks are completed and we don't need to include the idle time
+            time+=cycle; // this counts the idle time 
+        }
+        return time;
+    }
+};
+
+
+
+
+
+
+
+
 /*
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
+
         ios::sync_with_stdio(false);
         cin.tie(nullptr);
         cout.tie(nullptr);
-        if(tasks.size()==1) return 1;
-        if(n==0) return tasks.size();
 
-    }
-};
-*/
-
-
-
-
-
-
-
-
-
-class Solution {
-public:
-    int leastInterval(vector<char>& tasks, int n) {
-        ios::sync_with_stdio(false);
-        cin.tie(nullptr);
-        cout.tie(nullptr);
         int t_sz=tasks.size();
+
         if(t_sz==1) return 1;
         if(n==0) return tasks.size();
+
         int freq[26]={}, maxf=0;
+
         for(char c:tasks){
             ++freq[c-'A'];
             maxf=max(maxf,freq[c-'A']);
         }
+
         int gaps=(maxf-1)*(n+1);
+
         for(int el:freq){
             gaps-=min(maxf-1,el);
             if(gaps<=0) return t_sz;
         }
-        return t_sz+max(0,gaps);
+
+        return t_sz+gaps;
     }
 };
+*/
