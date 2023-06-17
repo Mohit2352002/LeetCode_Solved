269be4ed1d71991c172c660e1b99835ddbaedb11
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-06-18 01:59:14 +0530

    Time: 173 ms (86.98%), Space: 42.8 MB (42.01%) - LeetHub

--- 1290-make-array-strictly-increasing/1290-make-array-strictly-increasing.cpp
+++ 1290-make-array-strictly-increasing/1290-make-array-strictly-increasing.cpp
@@ -1,4 +1,59 @@
 class Solution {
+public:
+    vector<vector<int>> dp;
+    int n,m;
+    int help(int ind,int last,vector<int>& arr1, vector<int>& arr2){
+        if(ind>=n){
+            return 0;
+        }
+    
+        if(dp[ind][last]!=-1){
+            return dp[ind][last];
+        }
+
+        int t=1e7;
+        int lval;
+        if(last==0){
+            lval=arr1[ind-1];
+        }else{
+            lval=arr2[last-1];
+        }
+        if(arr1[ind]>lval){
+            t=help(ind+1,0,arr1,arr2);
+        }
+        int it=upper_bound(arr2.begin(),arr2.end(),lval)-arr2.begin();
+        if(it<arr2.size()){
+            t=min(t,1+help(ind+1,it+1,arr1,arr2));
+        }
+        
+        return dp[ind][last]=t;
+    }
+    int makeArrayIncreasing(vector<int>& arr1, vector<int>& arr2) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        n=arr1.size();
+        m =arr2.size();
+        sort(arr2.begin(),arr2.end());
+        dp.resize(n,vector<int>(m+1,-1));
+        int t=min(help(1,0,arr1,arr2),1+help(1,1,arr1,arr2) );
+        if(t>1e5){
+            return -1;
+        }
+        return t;
+    }
+};
+
+
+
+
+
+
+
+
+
+/*
+class Solution {
     map<pair<int,int>,int>mp;
     int solve(int idx, vector<int>& arr1, vector<int>& arr2, int prev){
         if(idx==arr1.size()) return 0;
@@ -24,3 +79,4 @@ public:
         return ans;
     }
 };
+*/
