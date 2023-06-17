780e011160b5b9e29f078f0f2e9fc7f2855dfe3b
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-06-18 01:16:41 +0530

    Time: 88 ms (99.51%), Space: 68 MB (20.75%) - LeetHub

--- 2810-collecting-chocolates/2810-collecting-chocolates.cpp
+++ 2810-collecting-chocolates/2810-collecting-chocolates.cpp
@@ -4,6 +4,45 @@ public:
         ios::sync_with_stdio(false);
         cin.tie(nullptr);
         cout.tie(nullptr);
+        long long cost, cost1, l = 0, r = nums.size();
+        while (l < r) {
+            long long m = (l + r) / 2;
+            cost = m * x, cost1 = (m + 1) * x;
+            deque<int> d; 
+            for (int i = 0; i < nums.size() + m + 1; ++i) {
+                while (!d.empty() && d.back() > nums[i % nums.size()])
+                    d.pop_back();
+                d.push_back(nums[i % nums.size()]);
+                if (i >= m + 1) {
+                    cost1 += d.front();
+                    if (d.front() == nums[i - m - 1])
+                        d.pop_front();
+                }
+                if (i >= m && i < nums.size() + m)
+                    cost += d.front();
+            }
+            if (cost > cost1)
+                l = m + 1;
+            else
+                r = m;
+        }
+        return min(cost, cost1);
+    }  
+};
+
+
+
+
+
+
+
+/*
+class Solution {
+public:
+    long long minCost(vector<int>& nums, int x) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
         int n = nums.size();
         int mincost[n];
         long long ans=0;
@@ -21,4 +60,4 @@ public:
         }
         return ans;
     }
-};
+};*/
