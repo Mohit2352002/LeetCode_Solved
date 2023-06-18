9faed77d12819fd7e9456b4d5cfdc057f282f414
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-06-19 00:29:19 +0530

    Time: 237 ms (98.31%), Space: 43.2 MB (93.1%) - LeetHub

--- 2409-number-of-increasing-paths-in-a-grid/2409-number-of-increasing-paths-in-a-grid.cpp
+++ 2409-number-of-increasing-paths-in-a-grid/2409-number-of-increasing-paths-in-a-grid.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    int mod = 1000000007;
+    int mod = 1000000007,n,m;
     int dx[4] = {1,0,-1,0};
     int dy[4] = {0,1,0,-1};
-    bool isvalid(int x, int y, vector<vector<int>>&grid){
-        if(x<0 or x>=grid.size() or y<0 or y>=grid[0].size()) return false;
+    bool isvalid(int x, int y){
+        if(x<0 or x>=n or y<0 or y>=m) return false;
         return true;
     }
     int dfs(int x, int y, vector <vector<int>>&grid,vector <vector<int>>&dp){
@@ -12,8 +12,8 @@ public:
         int ans = 1;
         for(int i = 0; i<4; i++){
             int nx = x + dx[i], ny = y + dy[i];
-            if(isvalid(nx,ny,grid) and grid[x][y]<grid[nx][ny]){
-                ans = (ans%mod+dfs(nx,ny,grid,dp)%mod)%mod;
+            if(isvalid(nx,ny) and grid[x][y]<grid[nx][ny]){
+                ans = (ans+dfs(nx,ny,grid,dp))%mod;
             }
         }
         return dp[x][y] = ans%mod;
@@ -24,6 +24,7 @@ public:
         cout.tie(nullptr);
         vector <vector <int>> dp(grid.size(),vector<int>(grid[0].size(),-1));
         long long count = 0;
+        n=grid.size(),m=grid[0].size();
         for(int i = 0; i<grid.size(); i++){
             for(int j = 0; j<grid[0].size(); j++){
                 count = (count + dfs(i,j,grid,dp))%mod;
