3cbec092389440518fab4a24f4136e8ca6e3ff6e
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-06-19 00:51:06 +0530

    Time: 270 ms (91.83%), Space: 43.1 MB (93.1%) - LeetHub

--- 2409-number-of-increasing-paths-in-a-grid/2409-number-of-increasing-paths-in-a-grid.cpp
+++ 2409-number-of-increasing-paths-in-a-grid/2409-number-of-increasing-paths-in-a-grid.cpp
@@ -1,35 +1,36 @@
 class Solution {
-public:
-    int mod = 1000000007,n,m;
-    int dx[4] = {1,0,-1,0};
-    int dy[4] = {0,1,0,-1};
+    int mod=1e9+7,n,m;
+    int dx[4]={1,0,-1,0};
+    int dy[4]={0,1,0,-1};
+    vector<vector<int>>dp;
     bool isvalid(int x, int y){
-        if(x<0 or x>=n or y<0 or y>=m) return false;
-        return true;
+        if(x>=0 and x<n and y>=0 and y<m) return true;
+        return false;
     }
-    int dfs(int x, int y, vector <vector<int>>&grid,vector <vector<int>>&dp){
+    int dfs(int x, int y, vector<vector<int>>& grid){
         if(dp[x][y]!=-1) return dp[x][y];
-        int ans = 1;
-        for(int i = 0; i<4; i++){
-            int nx = x + dx[i], ny = y + dy[i];
+        int ans=1;
+        for(int i=0;i<4;++i){
+            int nx=x+dx[i], ny=y+dy[i];
             if(isvalid(nx,ny) and grid[x][y]<grid[nx][ny]){
-                ans = (ans+dfs(nx,ny,grid,dp))%mod;
+                ans=(ans+dfs(nx,ny,grid))%mod;
             }
         }
-        return dp[x][y] = ans%mod;
+        return dp[x][y]=ans;
     }
+public:
     int countPaths(vector<vector<int>>& grid) {
         ios::sync_with_stdio(false);
         cin.tie(nullptr);
         cout.tie(nullptr);
-        vector <vector <int>> dp(grid.size(),vector<int>(grid[0].size(),-1));
-        long long count = 0;
-        n=grid.size(),m=grid[0].size();
-        for(int i = 0; i<grid.size(); i++){
-            for(int j = 0; j<grid[0].size(); j++){
-                count = (count + dfs(i,j,grid,dp))%mod;
+        n=grid.size(), m=grid[0].size();
+        dp.resize(n,vector<int>(m,-1));
+        long long count=0;
+        for(int i=0;i<n;++i){
+            for(int j=0;j<m;++j){
+                count=(count+dfs(i,j,grid))%mod;
             }
         }
-        return (int)count%mod;
+        return (int) count;
     }
 };
