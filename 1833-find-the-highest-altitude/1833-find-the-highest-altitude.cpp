194f7233acf8b89025b3e43e4deab22c4848b8f2
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-06-19 12:15:40 +0530

    Time: 6 ms (28.46%), Space: 8.1 MB (20.89%) - LeetHub

--- 1833-find-the-highest-altitude/1833-find-the-highest-altitude.cpp
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.cpp
@@ -7,7 +7,7 @@ public:
         int curr_alt=gain[0], ans=max(0,curr_alt), n=gain.size();
         for(int i=1;i<n;++i){
             curr_alt+=gain[i];
-            ans=max(ans,curr_alt);
+            if(ans<curr_alt) ans=curr_alt;
         }
         return ans;
     }
