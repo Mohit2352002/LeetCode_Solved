ab501e558dda665d65d22039fd7f768810c3c3c4
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-06-20 16:34:10 +0530

    Time: 215 ms (98.48%), Space: 8.2 MB (93.82%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,4 +1,5 @@
-#include<vector>
+/*
+// Recursion + Memoization (top down approach)
 class Solution {
     int dp[301][301];
     int helper(int start,int end,vector<int>& nums){
@@ -24,5 +25,49 @@ public:
         nums.insert(nums.begin(),1);
         nums.push_back(1);
         return helper(1,n,nums);
+    }
+};*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Tabulation (Bottom Up Approach)
+class Solution {
+public:
+    int dp[302][302];
+    int maxCoins(vector<int>& nums) {
+        std::ios_base::sync_with_stdio(false);
+        std::cout.tie(nullptr);
+        std::cin.tie(nullptr);
+        int n=nums.size();
+        if(n==1) return nums[0];
+        if(n==2) return nums[0]*nums[1]+max(nums[0],nums[1]);
+        memset(dp,0,sizeof(dp));
+        nums.insert(nums.begin(),1);
+        nums.push_back(1);
+        for(int start=n;start>=1;--start){
+            for(int end=start;end<=n;++end){
+                int maxi=INT_MIN;
+                for(int burst=start;burst<=end;++burst){
+                    maxi=max(maxi, nums[burst]*nums[start-1]*nums[end+1] +
+                   dp[start][burst-1]+ dp[burst+1][end]);
+                }
+                dp[start][end]=maxi; 
+            }
+        }
+        return dp[1][n];
     }
 };
