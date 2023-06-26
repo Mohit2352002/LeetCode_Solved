78b308cceb41a04ba6ca4782a4d2d1f11a563794
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-06-26 10:45:04 +0530

    Time: 247 ms (31.43%), Space: 11.3 MB (45.97%) - LeetHub

--- 1680-count-all-possible-routes/1680-count-all-possible-routes.cpp
+++ 1680-count-all-possible-routes/1680-count-all-possible-routes.cpp
@@ -1,3 +1,59 @@
+class Solution {
+
+    const int MOD = 1000000007;
+
+    int helper(vector<int>& nums, int start, int &finish, int fuel, vector<vector<int>>&dp){
+        
+        if(fuel<0) return 0;
+        if(dp[start][fuel]!=-1) return dp[start][fuel];
+
+        int total=0;
+        if(start==finish){
+            ++total;
+            if(fuel==0) return dp[start][0]=1;
+        }
+        if(fuel==0 and start!=finish) return 0;
+
+        for(int i=0;i<nums.size();++i){
+            if(nums[i]!=nums[start] and fuel>= abs(nums[start]-nums[i]) ){
+                total=(total+ helper(nums,i,finish,fuel-abs(nums[start]-nums[i]),dp) )%MOD;
+            }
+        }
+
+        return dp[start][fuel]=total;
+    }
+
+public:
+
+    int countRoutes(vector<int>& nums, int start, int finish, int fuel) {
+        
+        std::ios_base::sync_with_stdio(false);
+        std::cout.tie(nullptr);
+        std::cin.tie(nullptr);     
+
+        if((abs(nums[start]-nums[finish])) > fuel) return 0;
+
+        vector<vector<int>>dp(nums.size(),vector<int>(fuel+1,-1));
+
+        return helper(nums,start,finish,fuel,dp);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 class Solution {
 public:
@@ -41,7 +97,7 @@ public:
 
 
 
-
+/*
 class Solution {
 public:
     int countRoutes(vector<int>& locations, int start, int finish, int fuel) {  
@@ -71,3 +127,4 @@ public:
         return dp[start][fuel];
     }
 };
+*/
