802b5fc8b1332078a6a4b921a726bdf1dd3013f7
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-06-29 22:20:00 +0530

    Time: 59 ms (62.5%), Space: 8.3 MB (70.75%) - LeetHub

--- 0993-tallest-billboard/0993-tallest-billboard.cpp
+++ 0993-tallest-billboard/0993-tallest-billboard.cpp
@@ -5,21 +5,21 @@ class Solution {
 
 public:
     int helper(vector<int>&arr, int i, int firstSum,int secondSum){
-        
-      int diff = abs(firstSum-secondSum);
-       if(i==n){
-           if(diff==0){
+
+        int diff = abs(firstSum-secondSum);
+        if(i==n){
+            if(diff==0){
                return 0;
-           }
-           return -1e9;
-       }
+            }
+            return -1e9;
+        }
         if(dp[i][diff]!=-1){
             return dp[i][diff];
         }
         int ans1 = arr[i]+ helper(arr,i+1,firstSum+arr[i],secondSum);
         int ans2 = helper(arr,i+1,firstSum,secondSum);
-        int ans3 = arr[i]+ helper(arr,i+1,firstSum,secondSum+arr[i]);
-        return dp[i][diff] = max(ans1,max(ans2,ans3));
+        int ans3 = arr[i]+helper(arr,i+1,firstSum,secondSum+arr[i]);
+        return dp[i][diff] = max({ans1,ans2,ans3});
     }
     int tallestBillboard(vector<int>& rods) {
 
@@ -29,6 +29,7 @@ public:
         this->n = rods.size();
         memset(dp, -1, sizeof(dp));
         int ans = helper(rods,0,0,0);
+        if(ans<0) return 0;
         return ans/2;
     }
 };
