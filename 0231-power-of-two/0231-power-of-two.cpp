23842903d2200d908d9e692f9cc28bd78127a9f1
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-06-30 18:47:57 +0530

    Time: 5 ms (16.81%), Space: 6.1 MB (16.19%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -5,6 +5,27 @@ public:
         std::cout.tie(nullptr);
         std::cin.tie(nullptr);
         if(n<=0) return false;
-        return __builtin_popcountll(n)==1;
+        return (n&(n-1))==0;
     }
 };
+
+
+
+
+
+
+
+
+
+
+/*
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        std::ios_base::sync_with_stdio(false);
+        std::cout.tie(nullptr);
+        std::cin.tie(nullptr);
+        if(n<=0) return false;
+        return __builtin_popcountll(n)==1;
+    }
+};*/
