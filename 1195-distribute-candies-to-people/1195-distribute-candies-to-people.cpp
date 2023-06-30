1519a3d677c930ed5c7cd780aacbfcc5e6fdff38
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-06-30 18:43:22 +0530

    Time: 0 ms (100%), Space: 6.5 MB (16.46%) - LeetHub

--- 1195-distribute-candies-to-people/1195-distribute-candies-to-people.cpp
+++ 1195-distribute-candies-to-people/1195-distribute-candies-to-people.cpp
@@ -4,24 +4,21 @@ public:
         std::ios_base::sync_with_stdio(false);
         std::cout.tie(nullptr);
         std::cin.tie(nullptr);
+
         vector<int> res(num_people, 0);
 
         double total = floor((-1 + sqrt(1 + 8ll * candies)) / 2);
         int round = floor(total / num_people);
 
-        for (int i = 1; i <= num_people; i++)
-        {
+        for (int i = 1; i <= num_people; i++){
             int first = i;
             int last = first + num_people * (round - 1);
             int inComplete = candies - (num_people+last) * (num_people + last - 1) / 2;
-            res[i - 1] = (first + last) * round / 2 + max(min(last + num_people, inComplete), 0);
+            res[i - 1]= (first+last)*round/2 + max(min(last+num_people, inComplete), 0);
         }
-
         return res;
     }
 };
-
-
 
 
 
