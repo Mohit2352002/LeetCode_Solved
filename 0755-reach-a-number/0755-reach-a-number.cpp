b151a6628b07139719f57a4cd0cfb220fee1ec98
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-06-30 20:34:46 +0530

    Time: 0 ms (100%), Space: 6 MB (43.34%) - LeetHub

--- 0755-reach-a-number/0755-reach-a-number.cpp
+++ 0755-reach-a-number/0755-reach-a-number.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     int reachNumber(int target) {
+
         std::ios_base::sync_with_stdio(false);
         std::cout.tie(nullptr);
         std::cin.tie(nullptr);
-        if(target<0){
-            target=-target;
-        }
-        int step=0;
-        int sum=0;
-        while(true)
-        {
-            step++;
-            sum=sum+step;
-            if(sum==target)
-            {
-                return step;
-            }
-            else if(sum>target && (sum-target)%2==0)
-            {
-                return step;
-            }
+
+        if(target<0) target=-target;
+        int step=0,sum=0;
+        while(1){
+            ++step;
+            sum+=step;
+            if(sum==target) return step;
+            else if(sum>target && (sum-target)%2==0) return step;
         }
         return step;
     }
