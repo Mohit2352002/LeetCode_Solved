3b5910f46736aca28acd3a6cca396e6ff2ccc267
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-06-30 07:59:45 +0530

    Time: 0 ms (100%), Space: 5.9 MB (71.67%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -2,22 +2,26 @@ class Solution {
 public:
     bool isHappy(int n) {
         std::ios_base::sync_with_stdio(false);
+
         std::cout.tie(nullptr);
         std::cin.tie(nullptr);
-        if(n==1) return true;
-        else if(n==7) return true;
+
+        if(n==1 || n==7) return true;
         else if(n<10) return false;
-        long long int sum=0;
+
+        long long int sum;
+
         while(n>=10){
+            sum=0;
             while(n){
                 int digit= n%10;
                 n/=10;
                 sum+=1ll*digit*digit;
             }
             if(sum==1 || sum==7) return true;
-            n=sum;
-            sum=0;
+            else n=sum;
         }
+        
         return false;
     }
 };
