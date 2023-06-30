419ce59d16e22544fbb9dc4790c6aa36aca81784
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-06-30 08:02:46 +0530

    Time: 0 ms (100%), Space: 6.1 MB (62.49%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,10 +1,53 @@
+/*
+//Floyd cycle detection algo using slow and fast pointer
+class Solution {
+public:
+    int next(int n)
+    {
+        int sum = 0;
+        
+        while(n != 0)
+        {
+            sum += pow(n % 10,2);
+            n = n / 10;
+        }
+        
+        return sum;
+    }
+
+public:
+    bool isHappy(int n) {
+        
+        std::ios_base::sync_with_stdio(false);
+        std::cout.tie(nullptr);
+        std::cin.tie(nullptr);
+
+        int slow = next(n);
+        int fast = next(next(n));
+        
+        while(slow != fast)
+        {
+            slow = next(slow);
+            fast = next(next(fast));
+        }
+        
+        return fast == 1 ;
+    }
+};
+*/
+
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
     bool isHappy(int n) {
-        std::ios_base::sync_with_stdio(false);
-
-        std::cout.tie(nullptr);
-        std::cin.tie(nullptr);
 
         if(n==1 || n==7) return true;
         else if(n<10) return false;
@@ -21,7 +64,47 @@ public:
             if(sum==1 || sum==7) return true;
             else n=sum;
         }
-        
+
         return false;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_map<int,int> tmp;
+        
+        while(n != 1)
+        {
+            if(tmp[n] == 0)
+                tmp[n]++;
+            else
+                return false;
+            
+            int sum = 0;
+            while(n != 0)
+            {
+                sum += pow(n % 10,2);
+                n = n / 10;
+            }
+            
+            n = sum;
+        }
+        
+        return true;
+    }
+};
+*/
