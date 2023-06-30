f77e6c92face2adb06862e4da84a1b70d82ae3f5
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-06-30 19:11:02 +0530

    Time: 3 ms (68.4%), Space: 5.9 MB (58.53%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
-    int fib(int n) {
+    int fib(int N) {
         std::ios_base::sync_with_stdio(false);
         std::cout.tie(nullptr);
         std::cin.tie(nullptr);
-        if(n==1 || n==0) return n;
-        return fib(n-1)+fib(n-2);
+        if(N < 2)
+            return N;
+        int memo[N+1];
+        memo[0] = 0;
+        memo[1] = 1;
+        for(int i=2; i<=N; i++)
+            memo[i] = memo[i-1] + memo[i-2];
+        return memo[N];
     }
 };
