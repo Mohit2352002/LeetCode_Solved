21f666bbc9f957e82a7cb8cceb18e2922c3fcca9
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-06-30 18:46:38 +0530

    Time: 0 ms (100%), Space: 6 MB (16.19%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -4,6 +4,7 @@ public:
         std::ios_base::sync_with_stdio(false);
         std::cout.tie(nullptr);
         std::cin.tie(nullptr);
+        if(n<=0) return false;
         return __builtin_popcountll(n)==1;
     }
 };
