ae41e57499e0170a613200484421ae7b53291067
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-07-02 14:32:08 +0530

    Time: 290 ms (38.86%), Space: 9.2 MB (55.33%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -4,11 +4,10 @@ public:
         std::ios_base::sync_with_stdio(false);
         std::cout.tie(nullptr);
         std::cin.tie(nullptr);
-        if(n==0 || n==1 || n==2 || n==3) return n;
-        else if(n==4) return 1;
+        if(n<4) return n;
         vector<int>dp(n+1,INT_MAX);
-        dp[0]=0,dp[1]=1,dp[2]=2,dp[3]=3,dp[4]=1;
-        for(int i=5;i<=n;++i){
+        dp[0]=0,dp[1]=1,dp[2]=2,dp[3]=3;
+        for(int i=4;i<=n;++i){
             for(int j=1;j*j<=i;++j){
                 dp[i]=min(dp[i],dp[i-j*j]+1);
             }
