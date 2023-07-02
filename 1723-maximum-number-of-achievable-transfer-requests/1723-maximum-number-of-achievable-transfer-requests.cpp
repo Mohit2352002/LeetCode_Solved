d0f7ce0e1202be8513c19ad45f8d71197672a770
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-07-02 13:47:54 +0530

    Time: 94 ms (89.22%), Space: 8.8 MB (75.49%) - LeetHub

---
+++ 1723-maximum-number-of-achievable-transfer-requests/1723-maximum-number-of-achievable-transfer-requests.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    int ans=0,n;
+    void helper(int idx,vector<int>& count, int transfer, vector<vector<int>> &req){
+        if(idx==n){
+            for(int i:count) if(i!=0) return;
+            ans=max(ans,transfer);
+            return;
+        }
+        --count[req[idx][0]];
+        ++count[req[idx][1]];
+        helper(idx+1,count,transfer+1,req);
+        ++count[req[idx][0]];
+        --count[req[idx][1]];
+        helper(idx+1,count,transfer,req); //not include;
+    }
+public:
+    int maximumRequests(int n, vector<vector<int>>& requests) {
+        std::ios_base::sync_with_stdio(false);
+        std::cout.tie(nullptr);
+        std::cin.tie(nullptr);
+        this->n=requests.size();
+        vector<int>count(n,0);
+        helper(0,count,0,requests);
+        return ans;
+    }
+};
