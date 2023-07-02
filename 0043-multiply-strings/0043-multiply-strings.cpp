97da1a565ffd7d5323a2031f9b10be16c753bc8b
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-07-02 17:09:53 +0530

    Time: 3 ms (96.26%), Space: 6.6 MB (82.01%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.cpp
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -1,10 +1,11 @@
+/*
 class Solution {
 public:
     string multiply(string num1, string num2) {
         std::ios_base::sync_with_stdio(false);
         std::cout.tie(nullptr);
         std::cin.tie(nullptr);
-        
+
         if (num1 == "0" || num2 == "0") return "0";
         
         vector<int> res(num1.size()+num2.size(), 0);
@@ -23,5 +24,35 @@ public:
         while (i < res.size()) ans += to_string(res[i++]);
         
         return ans;
+    }
+};*/
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    string multiply(string a, string b){
+        std::ios_base::sync_with_stdio(false);
+        std::cout.tie(nullptr);
+        std::cin.tie(nullptr);
+        if (a=="0" || b=="0")  return "0";
+        int m = a.size() - 1, n = b.size() - 1, carry = 0;
+        string product;
+        for (int i=0; i<=m+n || carry; ++i) 
+        {
+            for (int j = max(0, i-n); j <= min(i, m); ++j)
+                carry += (a[m-j] - '0') * (b[n-i+j] - '0');
+            product += carry % 10 + '0';
+            carry /= 10;
+        }
+        reverse(begin(product), end(product));
+        return product;
     }
 };
