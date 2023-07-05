e8a742882f664f5ba913750333a309148a6bc9d4
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-07-05 16:00:40 +0530

    Time: 27 ms (99.84%), Space: 36.5 MB (65.98%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -4,6 +4,36 @@ public:
         std::ios_base::sync_with_stdio(false);
         std::cout.tie(nullptr);
         std::cin.tie(nullptr);
+        int i = 0, j = 0,last_zero_idx = -1,result = 0;
+        while(j < nums.size()) {
+            if(nums[j] == 0) {
+                i = last_zero_idx+1;
+                last_zero_idx = j;
+            }
+            result = max(result, j-i);
+            ++j;
+        }
+        return result;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+/*
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        std::ios_base::sync_with_stdio(false);
+        std::cout.tie(nullptr);
+        std::cin.tie(nullptr);
         int i=0, j=0, count=0, ans=0, n=nums.size();
         while(j<n){
             if(nums[j]==0) ++count;
@@ -15,7 +45,7 @@ public:
         }
     return ans;
     }
-};
+};*/
 
 
 
