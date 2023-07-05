1d9d9347d880799f9e7597983d80479c78455749
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-07-05 15:56:32 +0530

    Time: 28 ms (99.75%), Space: 36.4 MB (65.98%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -7,8 +7,8 @@ public:
         int i=0, j=0, count=0, ans=0, n=nums.size();
         while(j<n){
             if(nums[j]==0) ++count;
-            while(count>1){
-                if (nums[i++]==0) count--;
+            while(count==2){
+                if (nums[i++]==0) --count;
             }
             ans=max(ans,j-i);
             ++j;
