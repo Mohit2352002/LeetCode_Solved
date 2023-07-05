bda38e70ee5a193b4c06e642c81a7fd661e2c25e
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-07-05 20:50:03 +0530

    Time: 17 ms (56.07%), Space: 11.5 MB (18.79%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -9,6 +9,44 @@
  * };
  */
 class Solution {
+    void reverseNodes(ListNode* head, ListNode *end){
+        if(head==NULL) return;
+        ListNode*slow=NULL,*fast=head->next;
+        while(slow!=end){
+            head->next=slow;
+            slow=head;
+            head=fast;
+            if(fast!=NULL) fast=fast->next;
+        }
+    }
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+
+        std::ios_base::sync_with_stdio(false);
+        std::cout.tie(nullptr);
+        std::cin.tie(nullptr);
+
+        if(!head || !head->next || k==1) return head;
+
+        ListNode *start=head, *end=head;
+        int count=k-1;
+        while(count--){
+            end=end->next;
+            if(end==NULL) return head;
+        }
+
+        ListNode* nextHead=reverseKGroup(end->next,k);
+        reverseNodes(start,end);
+        start->next=nextHead;
+        return end;
+    }
+};
+
+
+
+
+/*
+class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
 
@@ -44,4 +82,4 @@ public:
 
         return dummy->next;
     }
-};
+};*/
