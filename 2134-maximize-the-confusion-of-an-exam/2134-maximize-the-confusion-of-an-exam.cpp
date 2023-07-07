fa8dbc676122f1ff1a9ed2e3cde4c1ac4ba181f2
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-07-07 11:24:20 +0530

    Time: 16 ms (99.08%), Space: 10 MB (95.61%) - LeetHub

--- 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.cpp
+++ 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.cpp
@@ -1,5 +1,37 @@
 class Solution {
 public:
+    int maxConsecutiveAnswers(string anskey, int k) {
+        std::ios_base::sync_with_stdio(false);
+        std::cout.tie(nullptr);
+        std::cin.tie(nullptr);
+        int i=0,j=0,n = anskey.length(),ctt=0,ctf=0,ans=0;
+        while(j<n){
+            if(anskey[j]=='T') ctt++;
+            else ctf++;
+            while(min(ctf,ctt)>k){
+                if(anskey[i]=='T') ctt--;
+                else ctf--;
+                i++;
+            }
+            ans =max(ans , j-i+1);
+            j++;
+        }
+        return ans ;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+/*
+class Solution {
+public:
     int maxConsecutiveAnswers(string s, int k) {
         std::ios_base::sync_with_stdio(false);
         std::cout.tie(nullptr);
@@ -36,4 +68,4 @@ public:
         }
         return ans;
     }
-};
+};*/
