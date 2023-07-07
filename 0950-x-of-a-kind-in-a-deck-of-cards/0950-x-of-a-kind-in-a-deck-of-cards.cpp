2ee78f62be31697f29ce74c7d2f1bc4614d750c8
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-07-07 20:52:37 +0530

    Time: 11 ms (94.5%), Space: 17.4 MB (80.68%) - LeetHub

--- 0950-x-of-a-kind-in-a-deck-of-cards/0950-x-of-a-kind-in-a-deck-of-cards.cpp
+++ 0950-x-of-a-kind-in-a-deck-of-cards/0950-x-of-a-kind-in-a-deck-of-cards.cpp
@@ -1,3 +1,27 @@
+int speedup = []{ios::sync_with_stdio(0); cin.tie(0); return 0; }();
+
+class Solution {
+    int cnt[10000];
+public:
+    bool hasGroupsSizeX(vector<int>& deck) {
+        for (int d : deck) cnt[d] = 0;
+        for (int d : deck) ++cnt[d];
+        int g = cnt[deck[0]];
+        if (g == 1) return false;
+        for (int d : deck) if ((g = gcd(g, cnt[d])) == 1) return false;
+        return true;
+    }
+};
+
+
+
+
+
+
+
+
+
+/*
 class Solution {
 public:
     bool hasGroupsSizeX(vector<int>& deck) {
@@ -10,4 +34,4 @@ public:
         for (auto el : count) res = __gcd(el.second, res);
         return res > 1;
     }
-};
+};*/
