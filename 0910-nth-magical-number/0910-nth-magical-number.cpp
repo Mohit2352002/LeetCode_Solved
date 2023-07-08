66c88205ba46bb217c71baa69fc1b84add0555db
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-07-08 23:44:21 +0530

    Time: 0 ms (100%), Space: 6.2 MB (12.55%) - LeetHub

---
+++ 0910-nth-magical-number/0910-nth-magical-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int nthMagicalNumber(int n, int a, int b) {
+        std::ios_base::sync_with_stdio(false);
+        std::cout.tie(nullptr);
+        std::cin.tie(nullptr);
+        long long low=min(a,b), high=n*low, lcm=(a*b)/__gcd(a,b), mod=1e9 + 7;
+        while(low<high){
+            long long mid=low+(high-low)/2, factor=(mid/a)+(mid/b)-(mid/lcm);
+            if(factor<n) low=mid+1;
+            else high=mid;
+        }
+        return low%mod;
+    }
+};
