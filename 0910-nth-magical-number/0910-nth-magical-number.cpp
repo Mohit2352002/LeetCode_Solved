d7e2574f4d01339b7074bc5118743ce450528881
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-07-08 23:51:40 +0530

    Time: 3 ms (45.39%), Space: 6 MB (40.96%) - LeetHub

--- 0910-nth-magical-number/0910-nth-magical-number.cpp
+++ 0910-nth-magical-number/0910-nth-magical-number.cpp
@@ -8,6 +8,38 @@ public:
         std::ios_base::sync_with_stdio(false);
         std::cout.tie(nullptr);
         std::cin.tie(nullptr);
+        long long low=min(a,b), high=n*low, lcm=(a*b)/gcd(a,b), mod=1e9 + 7,ans;
+        while(low<=high){
+            long long mid=low+(high-low)/2, factor=(mid/a)+(mid/b)-(mid/lcm);
+            if(factor<n) low=mid+1;
+            else{
+                ans=mid;
+                high=mid-1;
+            }
+        }
+        return ans%mod;
+    }
+};
+
+
+
+
+
+
+
+
+
+/*
+class Solution {
+    int gcd(int a, int b){
+        if(a==0) return b;
+        return gcd(b%a,a);
+    }
+public:
+    int nthMagicalNumber(int n, int a, int b) {
+        std::ios_base::sync_with_stdio(false);
+        std::cout.tie(nullptr);
+        std::cin.tie(nullptr);
         long long low=min(a,b), high=n*low, lcm=(a*b)/gcd(a,b), mod=1e9 + 7;
         while(low<high){
             long long mid=low+(high-low)/2, factor=(mid/a)+(mid/b)-(mid/lcm);
@@ -16,4 +48,4 @@ public:
         }
         return high%mod;
     }
-};
+};*/
