c2f5054a17e32771358586535ce83f5131f0dac2
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-07-10 09:32:43 +0530

    Time: 241 ms (99.1%), Space: 144.6 MB (57.99%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -9,6 +9,37 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ //DFS
+ class Solution {
+public:
+    int dfs(TreeNode* root) {
+        if (root == NULL) {
+            return 0;
+        }
+        if(!root->right){
+            return 1+dfs(root->left);
+        }else if(!root->left){
+            return 1+dfs(root->right);
+        }
+        return 1+min(dfs(root->right),dfs(root->left));
+    }
+    
+    int minDepth(TreeNode* root) {
+        std::ios_base::sync_with_stdio(false);
+        std::cout.tie(nullptr);
+        std::cin.tie(nullptr);
+        return dfs(root);
+    }
+};
+
+
+
+
+
+
+
+/*
+ //BFS
 class Solution {
 public:
     int minDepth(TreeNode* root) {
@@ -32,4 +63,4 @@ public:
         }
         return ans;
     }
-};
+};*/
