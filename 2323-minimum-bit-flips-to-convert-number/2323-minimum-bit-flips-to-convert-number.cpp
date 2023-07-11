9327214f7c967499454ae991c4ff55cf8030daaf
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-07-12 00:43:45 +0530

    Time: 4 ms (37.44%), Space: 6 MB (43.08%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
+        std::ios_base::sync_with_stdio(false);
+        std::cout.tie(nullptr);
+        std::cin.tie(nullptr);
+        long  x=start^goal,c=0;
+        while(x){
+            c+=x&1;
+            x=x>>1;
+        }
+        return c;
+    }
+};
+
+
+
+
+
+
+
+
+/*
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
         int ans=0;
         while(start || goal){
             int str_bit=start & 1, goal_bit=goal & 1;
@@ -10,4 +33,4 @@ public:
         }
         return ans;
     }
-};
+};*/
