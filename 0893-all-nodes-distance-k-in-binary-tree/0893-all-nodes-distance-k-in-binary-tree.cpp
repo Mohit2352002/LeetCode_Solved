5b2fd397e75df5d20937e0b2e3ff908176c3b105
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-07-11 14:56:44 +0530

    Time: 3 ms (93.43%), Space: 13.1 MB (55.27%) - LeetHub

--- 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
@@ -7,79 +7,10 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
- /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution {
-public:
-    void mark_parent(TreeNode* root, unordered_map<TreeNode*, TreeNode*> & parent){
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty()){
-            TreeNode* current = q.front();
-            q.pop();
-            if(current->left){
-                parent[current->left] = current;
-                q.push(current->left);
-            }
-            if(current->right){
-                parent[current->right] = current;
-                q.push(current->right);
-            }
-        }
-    }
-    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        std::ios_base::sync_with_stdio(false);
-        std::cout.tie(nullptr);
-        std::cin.tie(nullptr);
-        unordered_map<TreeNode*, TreeNode*> parent;
-        mark_parent(root, parent);
-        unordered_map<TreeNode*, bool> vis;
-        queue<TreeNode*> q;
-        q.push(target);
-        vis[target]=true;
-        int dis = 0;
-        while(!q.empty()){
-            int n = q.size();
-            if(dis==k) break;
-            dis++;
-            for(int i=0; i<n; i++){
-                TreeNode* curr = q.front();
-                q.pop();
-                if(curr->left && !vis[curr->left]){
-                    q.push(curr->left);
-                    vis[curr->left] = true;
-                }
-                if(curr->right && !vis[curr->right]){
-                    q.push(curr->right);
-                    vis[curr->right] = true;
-                }
-                if(parent[curr] && !vis[parent[curr]]){
-                    q.push(parent[curr]);
-                    vis[parent[curr]] = true;
-                }
-
-            }
-        }
-        vector<int> ans;
-        while(!q.empty()){
-            ans.push_back(q.front()->val);
-            q.pop();
-        }
-        return ans;
-    }
-};
-/*
-class Solution {
-    unordered_map<TreeNode*,TreeNode*>parent;
+    unordered_map<int,TreeNode*>parent;
     void findParent(TreeNode* node, TreeNode*par){
-        parent[node]=par;
+        parent[node->val]=par;
         if(node->left!=NULL) findParent(node->left,node);
         if(node->right!=NULL) findParent(node->right,node);
     }
@@ -109,9 +40,9 @@ public:
                     q.push(curr_node->right);
                     vis[curr_node->right]=true;
                 }
-                if(parent[curr_node] and !vis[parent[curr_node]]){ 
-                    q.push(parent[curr_node]);
-                    vis[parent[curr_node]]=true;
+                if(parent[curr_node->val] and !vis[parent[curr_node->val]]){ 
+                    q.push(parent[curr_node->val]);
+                    vis[parent[curr_node->val]]=true;
                 }
             }
             ++dist;
@@ -122,4 +53,4 @@ public:
         }
         return ans;
     }
-};*/
+};
