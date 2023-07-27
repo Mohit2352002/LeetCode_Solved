9e976654075f8b3978ef7ea0e1f7f61e9062dcbc
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-07-27 12:15:30 +0530

    Time: 823 ms (88.1%), Space: 178.3 MB (96.12%) - LeetHub

---
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -0,0 +1,66 @@
+static auto _ = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+struct Node{
+    Node* link[2];
+    bool containsKey(int ind){
+        return (link[ind]!=NULL);
+    }
+    Node* get(int ind){
+        return link[ind];
+    }
+    void put(int ind, Node* node){
+        link[ind]=node;
+    }
+};
+class Trie{
+    private: Node* root;
+    public:
+    Trie(){
+        root=new Node();
+    }
+    public:
+    void insert(int num){
+        Node* node=root;
+        for(int i=31;i>=0;--i){
+            int bit=((num>>i)&1);
+            if(!node->containsKey(bit)){
+                node->put(bit,new Node());
+            }
+            node=node->get(bit);
+        }       
+    }
+    public:
+    int findMax(int num){
+        Node* node=root;
+        int maxNum=0;
+        for(int i=31;i>=0;--i){
+            int bit=((num>>i)&1);
+            if(node->containsKey(1-bit)){
+                maxNum|=(1<<i);
+                node=node->get(1-bit);
+            }else{
+                node=node->get(bit);
+            }
+        }
+        return maxNum;
+    }
+};
+class Solution {
+public:
+    int findMaximumXOR(vector<int>& nums) {
+        Trie tr;
+        int n=nums.size();
+        for(int i=0;i<n;++i){
+            tr.insert(nums[i]);
+        }
+        int maxi=0;
+        for(int i=0;i<n;++i){
+            maxi=max(maxi,tr.findMax(nums[i]));
+        }
+        return maxi;
+    }
+};
