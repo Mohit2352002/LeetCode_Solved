da7cfe68993d1f54bcd13c2e72f02e739c4e1b75
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-07-27 10:53:01 +0530

    Time: 123 ms (99.64%), Space: 54.9 MB (100%) - LeetHub

---
+++ 2263-maximum-running-time-of-n-computers/2263-maximum-running-time-of-n-computers.cpp
@@ -0,0 +1,32 @@
+static auto _ = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+class Solution {
+    bool valid(int ans, vector<int>&nums, int &n, int &sz){
+        long long sum=min(ans,nums[0]);
+        for(int i=1;i<sz;++i){
+            sum+=min(ans,nums[i]);
+            if(sum>=(1ll*ans*n)) return true;
+        }
+        return false;
+    }
+public:
+    long long maxRunTime(int n, vector<int>& nums) {
+        long long sum=nums[0];
+        int sz=nums.size();
+        for(int i=1;i<sz;++i){
+            sum+=nums[i];
+        }
+        if(n==1) return sum;
+        long long high=sum/n, low=1;
+        while(low<high){
+            long long mid=low+((high-low)/2)+1;
+            if(valid(mid,nums,n,sz)) low=mid;
+            else high=mid-1;
+        }
+        return low;
+    }
+};
