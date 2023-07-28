28c15ab84e2f1bec061d5d7a3256a4ec7c2ed184
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-07-28 09:15:47 +0530

    Time: 0 ms (100%), Space: 7.3 MB (59.01%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -0,0 +1,35 @@
+static auto _ = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+class Solution {
+    bool helper(vector<int>&nums,int sum,int &k){
+        int count=1,curr=0;
+        for(int &el:nums){
+            if(curr+el<=sum){
+                curr+=el;
+            }else{
+                curr=el;
+                ++count;
+            }
+            if(count>k) return true;
+        }
+        return count>k;
+    }
+public:
+    int splitArray(vector<int>& nums, int k) {
+        int ans=INT_MAX,hi=0,low=0;
+        for(int &el:nums){
+            hi+=el;
+            low=max(low,el);
+        }
+        while(low<=hi){
+            int mid=low+((hi-low)/2);
+            if(helper(nums,mid,k)) low=mid+1;
+            else hi=mid-1;
+        }
+        return low;
+    }
+};
