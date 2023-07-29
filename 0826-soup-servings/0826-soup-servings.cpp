5b6a4ee3ff1c85c0e2ec5f7ec86c7793c72a77ad
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-07-29 11:39:10 +0530

    Time: 7 ms (63.88%), Space: 10.9 MB (50.95%) - LeetHub

--- 0826-soup-servings/0826-soup-servings.cpp
+++ 0826-soup-servings/0826-soup-servings.cpp
@@ -5,17 +5,17 @@ static auto _ = [](){
     return nullptr;
 }();
 class Solution {
-    map<pair<int,int>,double>mp;
+    unordered_map<int, unordered_map<int, double>> mp;
     double helper(int a, int b){
         if(a<=0 and b<=0) return 0.5;
         if(a<=0) return 1.0;
         if(b<=0) return 0.0;
-        if(mp.count({a,b})) return mp[{a,b}];
-        mp[{a,b}]+=helper(a-100,b);
-        mp[{a,b}]+=helper(a-75,b-25);
-        mp[{a,b}]+=helper(a-50,b-50);
-        mp[{a,b}]+=helper(a-25,b-75);
-        return mp[{a,b}]*=0.25;
+        if(mp[a].count(b)) return mp[a][b];
+        mp[a][b]=helper(a-100,b);
+        mp[a][b]+=helper(a-75,b-25);
+        mp[a][b]+=helper(a-50,b-50);
+        mp[a][b]+=helper(a-25,b-75);
+        return mp[a][b]*=0.25;
     }
 public:
     double soupServings(int n) {
