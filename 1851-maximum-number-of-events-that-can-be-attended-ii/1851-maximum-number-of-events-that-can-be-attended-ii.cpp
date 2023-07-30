cb59d8e2afea5c1df622bd4e82b067b45bd4ec27
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-07-30 20:09:14 +0530

    Time: 369 ms (23.74%), Space: 121.7 MB (5.01%) - LeetHub

--- 1851-maximum-number-of-events-that-can-be-attended-ii/1851-maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/1851-maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -1,14 +1,14 @@
+static auto _ = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
 class Solution {
 public:
-    int solve(vector<vector<int>>&dp, vector<vector<int>>& events, int n, int pos, int k)
-{
-        
-        if(pos >= n || k == 0 || pos==-1)
-            return 0;
-        
-        if(dp[pos][k] != -1)
-            return dp[pos][k];
-        
+    int solve(vector<vector<int>>&dp, vector<vector<int>>& events, int n, int pos, int k){
+        if(pos >= n || k == 0 || pos==-1) return 0;
+        if(dp[pos][k] != -1) return dp[pos][k];
         // finding the next event which we can attend
         int left=pos+1,right=n-1,i=-1;
         while(left<=right){
@@ -20,18 +20,13 @@ public:
                 left=mid+1;
             }
         }
-        
        return dp[pos][k] = max(solve(dp, events, n, pos+1, k), events[pos][2] + solve(dp, events, n, i, k-1));
-
     }
     
-    int maxValue(vector<vector<int>>& events, int k) 
-    {
+    int maxValue(vector<vector<int>>& events, int k){
         int n = events.size();
         sort(events.begin(), events.end());
-        
         vector<vector<int>>dp(n+1,vector<int>(k+1,-1));
-        
         return solve(dp, events, n, 0, k);
     }
 };
