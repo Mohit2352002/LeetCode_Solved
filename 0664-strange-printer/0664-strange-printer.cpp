6066d741eca3d8be5760417889ea71039023881c
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-07-30 13:21:17 +0530

    Time: 20 ms (93.88%), Space: 9.5 MB (27.92%) - LeetHub

--- 0664-strange-printer/0664-strange-printer.cpp
+++ 0664-strange-printer/0664-strange-printer.cpp
@@ -1,22 +1,45 @@
+/*
 class Solution {
 public:
     int strangePrinter(string s) {
-        int n=s.length();
-        vector<vector<int>>dp(n,vector<int>(n,0));
-        for(int i=n-1;i>=0;i--){
-            for(int j=i;j<n;j++){
-                if(i==j)dp[i][j]=1;
-                else{
-                    int temp=dp[i][j-1]+1,m;
-                    for(m=j-1;m>=i;m--)
-                        if(s[m]==s[j]){
-                            if(m>0&&temp>dp[i][m-1]+dp[m][j-1])temp=dp[i][m-1]+dp[m][j-1];
-                            else if(m==0) temp=dp[0][j-1];//for special cases
-                        }
-                    dp[i][j]=temp;
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        
+        for (int i=n-1; i>=0; i--){
+            dp[i][i] = 1;
+            for (int j=i+1; j<n; j++){
+                dp[i][j] = dp[i][j-1] + 1;
+                for (int k=i; k<j; k++){
+                    if (s[k] == s[j]) 
+                        dp[i][j] = min(dp[i][j], dp[i][k] + (k+1<=j-1 ? dp[k+1][j-1] : 0));
                 }
             }
         }
         return dp[0][n-1];
+    }
+};*/
+
+
+
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int solve(string& s, int i, int j){
+        if(i>j)             return 0;
+        if(dp[i][j]!=0)     return dp[i][j];
+        int x = i;
+        
+        while(x+1<=j && s[x]==s[x+1]) 
+            x++;
+        int ans = 1 + solve(s, x+1, j);
+        for(int k = x+1; k<=j; k++){
+            if(s[k] == s[i])
+                ans = min(ans, solve(s, k,j ) + solve(s, x+1, k-1));
+        }
+        return dp[i][j] = ans;
+    }
+    int strangePrinter(string s) {
+        dp = vector<vector<int>> (s.size(), vector<int>(s.size(), 0));
+        return solve(s, 0, s.size()-1);
     }
 };
