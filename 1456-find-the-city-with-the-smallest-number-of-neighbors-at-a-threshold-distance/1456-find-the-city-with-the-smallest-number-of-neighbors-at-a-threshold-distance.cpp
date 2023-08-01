55b3fd7571d54e03f97ebe03eec1fda2977da605
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-08-01 14:36:04 +0530

    Time: 105 ms (61.55%), Space: 35 MB (5.04%) - LeetHub

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -38,53 +38,35 @@ public:
     }
 };*/
 class Solution {
-public:
-    
-	// This return  the count of cities we can visit from source
-	// with distance at most equal to threshold
-    int bfs(vector<vector<pair<int, int>>>& adj, int source, int threshold, const int& n) {
-        int count = 0;
-        
-        queue<pair<int, int>> q;
-        vector<bool> vis(n, false);
-        q.push({source, 0});
-        
-        while(q.size()) {
-            int node = q.front().first;
-            int distance = q.front().second;
+    int bfs(vector<vector<pair<int,int>>>adj, int source, int n, int thres){
+        int count=0;
+        vector<bool>vis(n,false);
+        queue<pair<int,int>>q;
+        q.push({source,0});
+        while(!q.empty()){
+            pair<int,int> curr=q.front();
             q.pop();
-            vis[node] = true;
-            
-            if(distance > threshold) continue;
-            
-            for(auto& it : adj[node]) {
-                int adjNode = it.first;
-                int wt = it.second;
-                
-                if(!vis[adjNode] and wt + distance <= threshold){
-                    q.push({adjNode, wt + distance});
-                    count++;                
+            vis[curr.first]=true;
+            if(curr.second>thres) continue;
+            for(auto nghbr:adj[curr.first]){
+                if(!vis[nghbr.first] and (nghbr.second+curr.second)<=thres){
+                    q.push({nghbr.first,(nghbr.second+curr.second)});
+                    ++count;
                 }
             }
         }
-        
         return count;
     }
-    
+public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        
-        vector<vector<pair<int, int>>> adj(n);
-        
-        for(auto& edge : edges) {
-            adj[edge[0]].push_back({edge[1], edge[2]});
-            adj[edge[1]].push_back({edge[0], edge[2]});
+        vector<vector<pair<int,int>>>adj(n);
+        for(auto el:edges){
+            adj[el[0]].push_back({el[1],el[2]});
+            adj[el[1]].push_back({el[0],el[2]});
         }
-        
         int smallestNumOfNeighbours = n-1, city = n-1;
-        
-		// Get count for each city and return the one with smallest count
-        for(int i=0; i<n; i++) {
-            int count = bfs(adj, i, distanceThreshold, n); 
+        for(int i=0;i<n;++i){
+            int count=bfs(adj,i,n,distanceThreshold);
             if(smallestNumOfNeighbours >= count) {
                 smallestNumOfNeighbours = count;
                 city = i;
