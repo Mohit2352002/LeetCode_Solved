ab35294d82643e8bd09ecd686d13cc3165cc1bf6
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-08-02 19:43:03 +0530

    Time: 24 ms (6.35%), Space: 13.7 MB (25.49%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,31 @@
+static auto _ = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+class Solution {
+    vector<vector<int>>ans;
+    vector<int>temp;
+    set<vector<int>>s;
+    int n;
+    void helper(int start,vector<int>&nums){
+        if(start==n and s.find(temp)==s.end()) s.insert(temp);
+        for(int i=start;i<n;++i){
+            temp.push_back(nums[i]);
+            helper(i+1,nums);
+            temp.pop_back();
+            helper(i+1,nums);
+        }
+    }
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        n=nums.size();
+        sort(nums.begin(),nums.end());
+        helper(0,nums);
+        for(vector<int>el:s){
+            ans.push_back(el);
+        }
+        return ans;
+    }
+};
