7eda80b04fea40e4a46033501ddc909fc03bd3d3
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-08-02 12:18:09 +0530

    Time: 4 ms (48.23%), Space: 7.3 MB (51.27%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -7,19 +7,19 @@ static auto _ = [](){
 class Solution {
     int n;
     vector<vector<int>>ans;
-    void helper(vector<int>&nums,int start, vector<int>&temp){
+    vector<int>temp;
+    void helper(vector<int>&nums,int start){
         ans.push_back(temp);
         for(int i=start;i<n;++i){
             temp.push_back(nums[i]);
-            helper(nums,i+1,temp);
+            helper(nums,i+1);
             temp.pop_back();
         }
     }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         n=nums.size();
-        vector<int>temp;
-        helper(nums,0,temp);
+        helper(nums,0);
         return ans;
     }
 };
