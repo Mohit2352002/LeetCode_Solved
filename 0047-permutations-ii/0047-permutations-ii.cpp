be2a2fcc7ec9f78f62026b4d976e67c7aa8e5d48
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-08-02 21:21:56 +0530

    Time: 10 ms (65.8%), Space: 13.3 MB (18.52%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,17 +1,20 @@
 class Solution {
     int n;
     vector<vector<int>>ans;
-    void helper(int start, vector<int>nums){
+    void helper(int start, vector<int>&nums){
         if(start==(n-1)){
             ans.push_back(nums);
             return;
         }
+        unordered_set<int>s;
         helper(start+1,nums);
+        s.insert(nums[start]);
         for(int i=start+1;i<n;++i){
-            if(nums[i]==nums[start]) continue;
+            if(s.find(nums[i])!=s.end()) continue;
+            s.insert(nums[i]);
             swap(nums[i],nums[start]);
             helper(start+1,nums);
-            //swap(nums[i],nums[start]);
+            swap(nums[i],nums[start]);
         }
     }
 public:
