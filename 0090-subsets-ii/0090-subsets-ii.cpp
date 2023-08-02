93167d5ab5447cd9c2dc1b870e78289ccc345059
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-08-02 20:07:56 +0530

    Time: 5 ms (46.81%), Space: 7.8 MB (45.9%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -4,6 +4,7 @@ static auto _ = [](){
     cout.tie(nullptr);
     return nullptr;
 }();
+/*
 class Solution {
     vector<vector<int>>ans;
     vector<int>temp;
@@ -26,6 +27,68 @@ public:
         for(vector<int>el:s){
             ans.push_back(el);
         }
+        return ans;
+    }
+};*/
+
+
+
+
+
+
+
+/*
+class Solution {
+    vector<vector<int>>ans;
+    vector<int>temp;
+    int n;
+    void helper(int start,vector<int>&nums,bool prev_taken){
+        if(start==n){
+            ans.push_back(temp);
+            return;
+        }
+        helper(start+1,nums,false);
+        if(start>0 and nums[start]==nums[start-1] and prev_taken==false) return;
+        temp.push_back(nums[start]);
+        helper(start+1,nums,true);
+        temp.pop_back();
+        //helper(i+1,nums);
+    }
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        n=nums.size();
+        sort(nums.begin(),nums.end());
+        helper(0,nums,false);
+        return ans;
+    }
+};*/
+
+
+
+
+
+
+
+
+class Solution {
+    vector<vector<int>>ans;
+    vector<int>temp;
+    int n;
+    void helper(int start,vector<int>&nums){
+        ans.push_back(temp);
+        for(int i=start;i<n;++i){
+            if(i==start or nums[i]!=nums[i-1]){
+                temp.push_back(nums[i]);
+                helper(i+1,nums);
+                temp.pop_back();
+            }
+        }
+    }
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        n=nums.size();
+        sort(nums.begin(),nums.end());
+        helper(0,nums);
         return ans;
     }
 };
