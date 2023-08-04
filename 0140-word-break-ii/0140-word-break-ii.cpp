9806f400b0fd1b501f375b079c075b8c198d6715
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-08-04 12:42:59 +0530

    Time: 4 ms (30.53%), Space: 7.6 MB (28.24%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -0,0 +1,47 @@
+static auto _ = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+struct TrieNode{
+    bool isWord;
+    unordered_map<int,TrieNode*>children;
+    TrieNode(): isWord(false),children(unordered_map<int,TrieNode*>()){}
+};
+class Solution{
+    void helper(string &s,  int start,string &curr){
+        if(start>=s.size()) ans.push_back(curr);
+        for(int i=start;i<s.size();++i){
+            if(dict[s.substr(start,i-start+1)]){
+                int sz=curr.size();
+                if(sz!=0) curr+=" ";
+                curr+=s.substr(start,i-start+1);
+                helper(s,i+1,curr);
+                curr.erase(sz,s.substr(start,i-start+1).size()+1);
+            }
+        }
+    }
+    unordered_map<string,bool>dict;
+    vector<string>ans;
+public:
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        TrieNode*root=new TrieNode();
+        for(string word:wordDict){
+            dict[word]=true;
+            TrieNode*curr=root;
+            for(char ch:word){
+                if( curr->children.find(ch-'a') == curr->children.end() ){
+                    curr->children[ch-'a']=new TrieNode();
+                }
+                curr=curr->children[ch-'a'];
+            }
+            curr->isWord=true;
+        }
+        int n=s.size();
+        string temp="";
+        helper(s,0,temp);
+        return ans;
+    }
+};
+
