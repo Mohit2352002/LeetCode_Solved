ab66ecbe3067e8f99fedb28c444e6aea0f54c3ed
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-08-04 08:25:37 +0530

    Time: 18 ms (43.24%), Space: 15.5 MB (15.77%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -4,13 +4,12 @@ static auto _ = [](){
     cout.tie(nullptr);
     return nullptr;
 }();
+/*
 class Solution {
     unordered_map<string,bool>dict;
     int dp[301];
     bool helper(string &s, int start){
-        if(start==s.size()){
-            return true;
-        }
+        if(start==s.size()) return true;
         if(dp[start]!=-1) return dp[start];
         for(int i=start;i<s.size();++i){
             if(dict[s.substr(start,i-start+1)] and helper(s,i+1)) return dp[start]=true;
@@ -22,5 +21,41 @@ public:
         for(string word:wordDict) dict[word]=true;
         memset(dp,-1,sizeof dp);
         return helper(s,0);
+    }
+};//*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_map<string,bool>dict;
+        int dp[301],n=s.size();
+        memset(dp,0,sizeof dp);
+        for(string word:wordDict) dict[word]=true;
+        dp[n]=1;
+        for(int i=n-1;i>=0;--i){
+            for(int j=i;j<n;++j){
+                if(dict[s.substr(i,j-i+1)] and dp[j+1]){
+                    dp[i]=1;
+                    break;
+                }
+            }
+        }
+        return dp[0];
     }
 };
