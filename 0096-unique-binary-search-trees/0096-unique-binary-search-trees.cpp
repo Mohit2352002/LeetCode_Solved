ad3a69075c834994dd50a654820895c6862e1c8a
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-08-05 09:35:07 +0530

    Time: 2 ms (49.17%), Space: 6.1 MB (35.2%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -4,9 +4,9 @@ static auto _ = [](){
     cout.tie(nullptr);
     return nullptr;
 }();
+/*
 class Solution {
-    vector<int>dp;
-    int helper(int n){
+    /*int helper(int n){
         if(n<=1) return 1;
         if(dp[n]!=-1) return dp[n];
         int ans=0;
@@ -14,10 +14,36 @@ class Solution {
             ans+=(helper(i-1)*helper(n-i));
         }
         return dp[n]=ans;
-    }
+    }//*
 public:
     int numTrees(int n) {
-        dp.resize(n+1,-1);
-        return helper(n);
+        vector<int>dp(n+1,0);
+        dp[0]=dp[1]=1;
+        for(int i=2;i<=n;++i){
+            for(int root=1;root<=i;++root){
+                dp[i]+=(dp[root-1]*dp[i-root]);
+            }
+        }
+        return dp[n];
+    }
+};*/
+
+
+
+
+
+
+class Solution {
+public:
+    long ncr(int n, int r) {
+        long ans = 1;
+        for(int i = 0; i < r; i++) {
+            ans *= n-i;
+            ans /= i+1;
+        }
+        return ans;   
+    }
+    int numTrees(int n) {
+        return ncr(2*n, n) / (n + 1);
     }
 };
