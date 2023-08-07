864f22a044aaf151783b466b1009557803ed80ec
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-08-07 11:14:40 +0530

    Time: 0 ms (100%), Space: 9.3 MB (17.65%) - LeetHub

--- 0956-number-of-music-playlists/0956-number-of-music-playlists.cpp
+++ 0956-number-of-music-playlists/0956-number-of-music-playlists.cpp
@@ -1,3 +1,10 @@
+static auto _ = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+/*
 class Solution {
 public:
         int numMusicPlaylists(int N, int L, int K) {
@@ -13,5 +20,24 @@ public:
 
     long factorial(int n) {
         return n ? factorial(n - 1) * n % (long)(1e9 + 7) : 1;
+    }
+};*/
+
+class Solution {
+    int mod=1e9+7;
+    vector<vector<long long>>dp;
+    int solve(int count_song, int count_unique, int &n, int &goal, int &k){
+        if(count_song==goal) return count_unique==n;
+        if(dp[count_song][count_unique]!=-1) return dp[count_song][count_unique];
+        long long res=0;
+        if(count_unique<n) res+=(1ll*(n-count_unique)*solve(count_song+1,count_unique+1,n,goal,k));
+        if(count_unique>k) res+=(1ll*(count_unique-k)*solve(count_song+1,count_unique,n,goal,k));
+        return dp[count_song][count_unique]=res%mod;
+    }
+public:
+    int numMusicPlaylists(int n, int goal, int k) {
+        if(n==1) return 1;
+        dp.resize(goal+1,vector<long long>(n+1,-1));
+        return solve(0,0,n,goal,k);
     }
 };
