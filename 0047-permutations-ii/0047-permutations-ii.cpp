9f3a718fe0ca613afab78d62a426c75246cc1f9c
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-08-08 20:19:07 +0530

    Time: 3 ms (96.57%), Space: 8.5 MB (86.72%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,3 +1,17 @@
+static auto _ = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+
+
+
+
+
+
+
+/*
 class Solution {
     int n;
     vector<vector<int>>ans;
@@ -23,5 +37,70 @@ public:
         sort(nums.begin(),nums.end());
         helper(0,nums);
         return ans;
+    }
+};
+*/
+
+
+
+
+
+
+
+/*
+class Solution {
+public:
+    void recursion(vector<int> num, int i, int j, vector<vector<int> > &res) {
+        if (i == j-1) {
+            res.push_back(num);
+            return;
+        }
+        for (int k = i; k < j; k++) {
+            if (i != k && num[i] == num[k]) continue;
+            swap(num[i], num[k]);
+            recursion(num, i+1, j, res);
+        }
+    }
+    vector<vector<int> > permuteUnique(vector<int> &num) {
+        sort(num.begin(), num.end());
+        vector<vector<int> >res;
+        recursion(num, 0, num.size(), res);
+        return res;
+    }
+};*/
+
+
+
+
+
+
+
+
+
+
+class Solution {
+private: 
+    void permuteUnique(vector<int>& nums, vector<vector<int>>& output, vector<int>& temp, vector<bool>& visited){
+        if(temp.size() == nums.size()){
+            output.push_back(temp);
+            return;
+        }
+        for(int i=0; i<nums.size(); i++){
+            if(visited[i] || i>0 && nums[i] == nums[i-1] && !visited[i-1]) continue;
+            visited[i] = true;
+            temp.push_back(nums[i]);
+            permuteUnique(nums, output, temp, visited);
+            temp.pop_back();
+            visited[i] = false;
+        }
+    }
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> output;
+        vector<int> temp;
+        vector<bool> visited(nums.size(), 0); 
+        permuteUnique(nums, output, temp, visited);
+        return output;
     }
 };
