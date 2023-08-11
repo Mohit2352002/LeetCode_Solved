3713256aa277af5208647e51521880bb814d359b
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-08-11 14:11:26 +0530

    Time: 2 ms (38.98%), Space: 6 MB (58.05%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -6,15 +6,17 @@ static auto _ = [](){
 }();
 
 class Solution {
-    int dp[46];
 public:
     int climbStairs(int n) {
-        dp[0]=1;
-        dp[1]=1;
-        dp[2]=2;
-        for(int i=3;i<=n;++i){
-            dp[i]=dp[i-1]+dp[i-2];
+        if(n <= 0) return 0;
+        else if(n == 1) return 1;
+        else if(n == 2) return 2;
+        int one_step_before = 2,two_steps_before = 1,all_ways = 0;
+        for(int i=3; i<=n; i++){
+            all_ways = one_step_before + two_steps_before;
+            two_steps_before = one_step_before;
+            one_step_before = all_ways;
         }
-        return dp[n];
+        return all_ways;
     }
 };
