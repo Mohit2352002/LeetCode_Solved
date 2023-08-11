643a82214172d496161b3f74631f8ac76e711b42
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-08-11 12:21:30 +0530

    Time: 4 ms (68.94%), Space: 14.2 MB (19.16%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -8,29 +8,19 @@ static auto _ = [](){
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        int l = 0,r = nums.size() - 1;
-        
-        while(l <= r){
-            int mid = l + ((r-l) >>1);
-            if (nums[mid] == target)  return true;
-			// with duplicates we can have this contdition, just update left & right
-            if((nums[l] == nums[mid]) && (nums[r] == nums[mid])){
-                l++;
-                r--;
-            }
-			// first half
-			// first half is in order
-            else if(nums[l] <= nums[mid]){
-				// target is in first  half
-                if((nums[l] <= target) && (nums[mid] > target))  r = mid - 1;
-                else  l = mid + 1;
-            }
-			// second half
-			// second half is order
-			// target is in second half
-            else{
-                if((nums[mid] < target) && (nums[r]>= target))  l = mid + 1;
-                else  r = mid - 1;
+        int low=0,high=nums.size()-1;
+        while(low<=high){
+            int mid=low+((high-low)>>1);
+            if(nums[mid]==target) return true;
+            else if(nums[mid]==nums[high] and nums[low]==nums[mid]){
+                ++low;
+                --high;
+            }else if(nums[mid]>=nums[low]){
+                if(nums[mid]>=target and nums[low]<=target) high=--mid;
+                else low=++mid;
+            }else{
+                if(nums[mid]<=target and nums[high]>=target) low=++mid;
+                else high=--mid;
             }
         }
         return false;
