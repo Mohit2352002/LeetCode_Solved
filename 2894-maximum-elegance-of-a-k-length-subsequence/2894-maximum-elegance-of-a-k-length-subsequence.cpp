55d9acd0cd1de028e13c99e9080d95ca9a07b561
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-08-13 12:12:17 +0530

    Time: 375 ms (98.5%), Space: 179.9 MB (68.65%) - LeetHub

--- 2894-maximum-elegance-of-a-k-length-subsequence/2894-maximum-elegance-of-a-k-length-subsequence.cpp
+++ 2894-maximum-elegance-of-a-k-length-subsequence/2894-maximum-elegance-of-a-k-length-subsequence.cpp
@@ -8,27 +8,25 @@ class Solution {
 public:
     long long findMaximumElegance(vector<vector<int>>& items, int k) {
         int n=items.size();
-        if(n==1) return items[0][0] +1;
+        if(n==1) return items[0][0]+1;
         sort(items.begin(),items.end(),[&](vector<int>&a,vector<int>&b){
             return a[0]>b[0];
         });
-        long long res = 0, cur = 0;
-        vector<int> dup;
-        unordered_set<int> seen;
-        for (int i = 0; i < items.size(); ++i) {
-            if (i < k) {
-                if (seen.count(items[i][1])) {
-                    dup.push_back(items[i][0]);
-                }
-                cur += items[i][0];
-            } else if (seen.find(items[i][1]) == seen.end()) {
-                if (dup.empty()) break;
-                cur += items[i][0] - dup.back();
+        long long ans=0,cur=0;
+        vector<int>dup;
+        unordered_set<int>seen;
+        for(int i=0;i<n;++i){
+            if(i<k){
+                cur+=items[i][0];
+                if(seen.find(items[i][1])!=seen.end()) dup.push_back(items[i][0]);
+            }else if(seen.find(items[i][1])==seen.end()){
+                if(dup.empty()) break;
+                cur+=(items[i][0]-dup[dup.size()-1]);
                 dup.pop_back();
             }
             seen.insert(items[i][1]);
-            res = fmax(res, cur + 1L * seen.size() * seen.size());
+            ans=fmax(ans,cur+(1ll*seen.size()*seen.size()));
         }
-        return res;
+        return ans;
     }
 };
