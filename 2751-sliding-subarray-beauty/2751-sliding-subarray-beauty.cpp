d503c4189ad34e06bc4888503f088d5aab07687f
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-08-13 09:09:55 +0530

    Time: 313 ms (94.77%), Space: 185.9 MB (70.51%) - LeetHub

---
+++ 2751-sliding-subarray-beauty/2751-sliding-subarray-beauty.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> getSubarrayBeauty(vector<int>& nums, int k, int x) {
+        vector<int>ans;
+        vector<int>freq(51,0);
+        for(int i=0;i<k;++i){
+            if(nums[i]<0) ++freq[-nums[i]];
+        }
+        int count=0,n=nums.size(),flag=0;
+        for(int i=50;i>0;--i){
+            count+=freq[i];
+            if(count>=x){
+                ans.push_back(-i);
+                flag=1;
+                break;
+            }
+        }
+        if(!flag) ans.push_back(0);
+        for(int i=k;i<n;++i){
+            if(nums[i-k]<0) --freq[-nums[i-k]];
+            if(nums[i]<0) ++freq[-nums[i]];
+            count=0;
+            flag=0;
+            for(int i=50;i>0;--i){
+                count+=freq[i];
+                if(count>=x){
+                    ans.push_back(-i);
+                    flag=1;
+                    break;
+                }
+            }
+            if(!flag) ans.push_back(0);
+        }
+        return ans;
+    }
+};
