ed0b2dcc7b135e5e56d22d0fc2e96ae335c62cad
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-08-13 08:44:58 +0530

    Time: 4 ms (44.19%), Space: 6.1 MB (13.7%) - LeetHub

--- 2752-sum-multiples/2752-sum-multiples.cpp
+++ 2752-sum-multiples/2752-sum-multiples.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     int sumOfMultiples(int n) {
@@ -6,5 +7,16 @@ public:
             if(i%3 == 0 || i%5 == 0|| i%7 == 0) sum += i;
         }
         return sum;
+    }
+};*/
+
+
+class Solution {
+public:
+    int cal(int n, int k){ 
+        return k *(n/k)*((n/k)+1)/2;
+        }
+    int sumOfMultiples(int n) {
+        return cal(n, 3) + cal(n, 5) + cal(n, 7) - cal(n, 3*5) - cal(n, 5*7) - cal(n, 3*7) + cal(n, 3*5*7);
     }
 };
