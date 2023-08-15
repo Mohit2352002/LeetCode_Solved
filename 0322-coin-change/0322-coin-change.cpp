a113870a35d1ce0b9209fec78b6a3c6a6a02226b
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-08-15 22:18:23 +0530

    Time: 250 ms (7%), Space: 90.5 MB (5.02%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,3 +1,11 @@
+static auto _ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+
+
 class Solution {
     int n,ans=1e6;
     vector<vector<int>>dp;
