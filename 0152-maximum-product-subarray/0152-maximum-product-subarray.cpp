4a08deefcc6d4692f95b1a42f3c022d381ab03d9
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-08-16 20:58:45 +0530

    Time: 4 ms (79.39%), Space: 13.9 MB (7.28%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,3 +1,11 @@
+static auto _ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+
+
 /*
 class Solution {
 public:
@@ -19,27 +27,17 @@ public:
 
 class Solution {
 public:
-    int maxProduct(vector<int>& v) {
+    int maxProduct(vector<int>& nums) {
         ios::sync_with_stdio(false);
         cin.tie(nullptr);
         cout.tie(nullptr);
-        
-        int n = v.size();
-        int ans = v[0], p = 1;
-        for(int i = 0;i<n;i++){
-            p*=v[i];
-            ans = max(ans, p);
-            if (p==0){
-                p = 1;
-            }
-        }
-        p = 1;
-        for(int i=n-1;i>=0;i--){
-            p*=v[i];
-            ans = max(ans, p);
-            if (p==0){
-                p = 1;
-            }
+        int n=nums.size(),ans=nums[0],prefix=1,suffix=1;
+        for(int i=0;i<n;++i){
+            if(suffix==0) suffix=1;
+            if(prefix==0) prefix=1;
+            prefix*=nums[i];
+            suffix*=nums[n-i-1];
+            ans=max({ans,suffix,prefix});
         }
         return ans;
     }
