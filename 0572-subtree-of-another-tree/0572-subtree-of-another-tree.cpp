d0dd038d0f2d23bf732e313d10c799300190472b
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-08-17 00:06:47 +0530

    Time: 20 ms (78.89%), Space: 28.8 MB (31.83%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -11,8 +11,8 @@
  */
 class Solution {
     bool isSame(TreeNode *s, TreeNode *t){
-        if (!s && !t) return true;
-        if (!s || !t) return false;
+        if (s==NULL && t==NULL) return true;
+        if (s==NULL || t==NULL) return false;
         if (s->val!=t->val) return false;
         return isSame(s->left,t->left) && isSame(s->right,t->right);
     }
