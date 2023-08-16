659fbaf8b41f73b766a0e7c698ae7fcb5e0ad691
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-08-16 17:23:41 +0530

    Time: 3 ms (94.58%), Space: 13.9 MB (19.11%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        int n=nums.size(),ans=nums[0],imax=nums[0],imin=nums[0];
+        for(int i=1;i<n;++i){
+            if(nums[i]<0) swap(imin,imax);
+            imax=max(nums[i],imax*nums[i]);
+            imin=min(nums[i],imin*nums[i]);
+            ans=max(ans,imax);
+        }
+        return ans;
+    }
+};
