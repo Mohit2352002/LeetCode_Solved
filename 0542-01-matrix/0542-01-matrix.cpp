75063771324d0c9f4d7bf2aa43d9224d45b7cec9
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-08-17 11:21:25 +0530

    Time: 74 ms (74.95%), Space: 30.3 MB (81.53%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -7,6 +7,7 @@ static auto _ = [](){
 
 
 
+/*
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
@@ -35,5 +36,41 @@ public:
             }
         }
         return mat;
+    }
+};*/
+
+
+
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int m=mat.size(),n=mat[0].size();
+        vector<vector<int>>ans(m,vector<int>(n,-1));
+        queue<pair<int,int>>q;
+        for(int i=0;i<m;++i){
+            for(int j=0;j<n;++j){
+                if(mat[i][j]==0){
+                    ans[i][j]=0;
+                    q.push({i,j});
+                }
+            }
+        }
+        vector<int>dir={1,0,-1,0,1};
+        while(!q.empty()){
+            int sz=q.size();
+            while(sz--){
+                auto [i,j]=q.front();
+                q.pop();
+                for(int k=0;k<4;++k){
+                    if(i+dir[k]>=0 and i+dir[k]<m and j+dir[k+1]>=0 and j+dir[k+1]<n){
+                        if(ans[i+dir[k]][j+dir[k+1]]==-1){
+                            ans[i+dir[k]][j+dir[k+1]]=1+ans[i][j];
+                            q.push({i+dir[k],j+dir[k+1]});
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
     }
 };
