5804e3479cbac1fd68c3abf98ff1a8d41d48c66e
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-08-17 20:42:38 +0530

    Time: 0 ms (100%), Space: 11.1 MB (89.48%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,3 +1,11 @@
+static auto _ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+
+/*
 class Solution {
     vector<vector<int>>dp;
     int helper(vector<int>&nums,int i,bool buy){
@@ -10,5 +18,29 @@ public:
     int maxProfit(vector<int>& prices) {
         dp.resize(2,vector<int>(prices.size(),-1));
         return helper(prices,0,true);
+    }
+};*/
+
+
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int dp[n+2][2];
+        for(int day=n+1;day>=0;--day){
+            for(int buy=0;buy<=1;++buy){
+                int &ans=dp[day][buy];
+                if(day>=n) ans=0;
+                else{
+                    int ans1=dp[day+1][buy],ans2=0;
+                    if(buy) ans2=-prices[day]+dp[day+1][0];
+                    else ans2=prices[day]+dp[day+2][1];
+                    ans=max(ans1,ans2);
+                }
+            }
+        }
+        return dp[0][1];
     }
 };
