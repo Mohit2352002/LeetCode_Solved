9cd1ada92d017b2c6b7890aead87b7e9fd98380d
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-08-17 11:58:56 +0530

    Time: 0 ms (100%), Space: 6.6 MB (5.36%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -6,6 +6,9 @@ static auto _ = [](){
 }();
 
 
+
+
+/*
 class Solution {
     vector<int>dir={1,0,1};
     vector<vector<int>>dp;
@@ -25,5 +28,23 @@ public:
     int uniquePaths(int m, int n) {
         dp.resize(m,vector<int>(n,-1));
         return dfs(0,0,m,n);
+    }
+};*/
+
+
+
+
+
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m,vector<int>(n,1));
+        for(int i=m-2;i>=0;--i){
+            for(int j=n-2;j>=0;--j){
+                dp[i][j]=dp[i+1][j]+dp[i][j+1];
+            }
+        }
+        return dp[0][0];
     }
 };
