fe2cf9e162221279b428751fdc0e2b8844622e92
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-08-24 09:00:09 +0530

    Time: 35 ms (94.02%), Space: 5.9 MB (39.64%) - LeetHub

--- 2541-sum-of-number-and-its-reverse/2541-sum-of-number-and-its-reverse.cpp
+++ 2541-sum-of-number-and-its-reverse/2541-sum-of-number-and-its-reverse.cpp
@@ -1,3 +1,10 @@
+static auto _ = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+
 class Solution {
     int reverse(int n){
         int ans=0;
@@ -10,7 +17,7 @@ class Solution {
 public:
     bool sumOfNumberAndReverse(int num) {
         if(num==0) return true;
-        for(int i=0;i<num;++i){
+        for(int i=num/2;i<num;++i){
             if(i+reverse(i)==num) return true;
         }
         return false;
