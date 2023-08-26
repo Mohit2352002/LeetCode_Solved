6973d0b3e489714915c705f58c60d3b832617d52
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-08-26 10:10:02 +0530

    Time: 19 ms (95.63%), Space: 12.8 MB (79.02%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -5,6 +5,8 @@ static auto _ = [](){
     return nullptr;
 }();
 
+
+/*
 class Solution {
     vector<vector<int>>dp;
     int helper(string &s,string&t, int &n,int &sz, int i,int j){
@@ -22,5 +24,23 @@ public:
         if(n==sz) return s==t;
         dp.resize(n,vector<int>(sz,-1));
         return helper(s,t,n,sz,0,0);
+    }
+};*/
+
+
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int n=s.size(),sz=t.size();
+        if(n<sz) return 0;
+        if(n==sz) return s==t;
+        vector<vector<unsigned int>>dp(sz+1,vector<unsigned int>(n+1,0));
+        for(int j=0;j<=n;++j) dp[0][j]=1;
+        for(int i=1;i<=sz;++i){
+            for(int j=1;j<=n;++j){
+                dp[i][j]= dp[i][j-1] + ((t[i-1]==s[j-1])? dp[i-1][j-1]:0);
+            }
+        }
+        return dp[sz][n];
     }
 };
