625ab293199366c270272093ca53e63b8e557b89
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-08-27 21:07:20 +0530

    Time: 147 ms (61.4%), Space: 41 MB (56.42%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -5,6 +5,9 @@ static auto _ = [](){
     return nullptr;
 }();
 
+
+
+/*
 class Solution {
     int n;
     vector<vector<int>>dp;
@@ -13,31 +16,52 @@ class Solution {
         if(jump<=0 or jump>(nums[n-1]-nums[i])) return false;
         if(dp[i][jump]!=-1) return dp[i][jump];
         int ans=false;
-        int low=i+1,high=n-1,flag=0;
+        int low=i+1,high=n-1;
         while(low<=high){
             int mid=(low+((high-low)>>1));
             if(nums[mid]==(nums[i]+jump)){
-                low=mid;
-                flag=1;
+                ans=(ans || helper(nums,mid,jump));
+                ans=(ans || helper(nums,mid,jump+1));
+                ans=(ans || helper(nums,mid,jump-1));
                 break;
             }
             else if(nums[mid]>(nums[i]+jump)) high=mid-1;
             else low=mid+1;
         }
-        if(flag){
-            ans=(ans || helper(nums,low,jump));
-            ans=(ans || helper(nums,low,jump+1));
-            ans=(ans || helper(nums,low,jump-1));
-        }
         return dp[i][jump]=ans;
     }
 public:
     bool canCross(vector<int>& stones) {
-        n=stones.size();
         if(stones[1]!=1) return false;
+        n=stones.size();
         dp.resize(n,vector<int>(n,-1));
         int ans=helper(stones,1,1);
         ans=(ans || helper(stones,1,2));
         return ans;
+    }
+};*/
+
+
+
+
+class Solution {
+public:
+    bool canCross(std::vector<int>& stones) {
+        int n = stones.size();
+        unordered_map<int,unordered_set<int>> dp;
+        for (int stone : stones) {
+            dp[stone] = unordered_set<int>();
+        }
+        dp[0].insert(0);
+        for (int i = 0; i < n; i++) {
+            for (int k : dp[stones[i]]) {
+                for (int step = k - 1; step <= k + 1; step++) {
+                    if (step != 0 && dp.count(stones[i] + step)) {
+                        dp[stones[i] + step].insert(step);
+                    }
+                }
+            }
+        }
+        return !dp[stones.back()].empty();
     }
 };
