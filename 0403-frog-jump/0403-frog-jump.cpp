64528d0c32d143f91af5d7482dafe8959d65bb09
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-08-27 20:47:23 +0530

    Time: 79 ms (81.98%), Space: 210.6 MB (30.88%) - LeetHub

---
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    int n;
+    vector<vector<int>>dp;
+    bool helper(vector<int>&nums, int i,int jump){
+        if(i>=n-1 or jump==(nums[n-1]-nums[i])) return true;
+        if(jump<=0 or jump>(nums[n-1]-nums[i])) return false;
+        if(dp[i][jump]!=-1) return dp[i][jump];
+        int ans=false;
+        for(int j=i+1;j<n;++j){
+            if(nums[j]==(nums[i]+jump)){
+                ans=(ans || helper(nums,j,jump));
+                ans=(ans || helper(nums,j,jump+1));
+                ans=(ans || helper(nums,j,jump-1));
+                break;
+            }
+        }
+        return dp[i][jump]=ans;
+    }
+public:
+    bool canCross(vector<int>& stones) {
+        n=stones.size();
+        if(stones[1]!=1) return false;
+        dp.resize(n,vector<int>(n,-1));
+        int ans=helper(stones,1,1);
+        ans=(ans || helper(stones,1,2));
+        return ans;
+    }
+};
