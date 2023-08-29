712cbeec9d786da4037b2e6080c01982b4448121
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-08-29 09:21:37 +0530

    Time: 0 ms (100%), Space: 6.9 MB (25.78%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -5,6 +5,9 @@ static auto _ = [](){
     return nullptr;
 }();
 
+
+
+/*
 class MyStack {
 	queue<int>q;
 public:
@@ -33,6 +36,34 @@ public:
     
     bool empty() {
 			return q.size()==0;
+    }
+};*/
+
+
+
+class MyStack {
+    queue<int>q1,q2;
+public:
+    MyStack() {
+    }
+    void push(int x) {
+        q2.push(x);
+        while(!q1.empty()){
+            q2.push(q1.front());
+            q1.pop();
+        }
+        swap(q1,q2);
+    }
+    int pop() {
+        int x=q1.front();
+        q1.pop();
+        return x;
+    }
+    int top() {
+        return q1.front();
+    }
+    bool empty() {
+        return q1.size()==0;
     }
 };
 
