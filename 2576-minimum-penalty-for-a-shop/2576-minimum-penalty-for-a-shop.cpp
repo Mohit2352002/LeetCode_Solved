60b549d9871327854fe0fc767ae9a57736de70cd
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-08-29 12:15:25 +0530

    Time: 26 ms (71.04%), Space: 18.9 MB (26.64%) - LeetHub

---
+++ 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.cpp
@@ -0,0 +1,31 @@
+static auto _ = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+
+class Solution {
+public:
+    int bestClosingTime(string s) {
+        int n=s.size();
+        vector<int>visits(n+1),absent(n+1);
+        visits[n]=0;
+        for(int i=n-1;i>=0;--i){
+            visits[i]=(s[i]=='Y');
+            if(i!=n-1) visits[i]+=visits[i+1];
+        }
+        absent[0]=0;
+        for(int i=1;i<=n;++i){
+            absent[i]=absent[i-1]+(s[i-1]=='N');
+        }
+        int penalty=INT_MAX, closing=0;
+        for(int i=0;i<=n;++i){
+            if(penalty>(absent[i]+visits[i])){
+                closing=i;
+                penalty=absent[i]+visits[i];
+            }
+        }
+        return closing;
+    }
+};
