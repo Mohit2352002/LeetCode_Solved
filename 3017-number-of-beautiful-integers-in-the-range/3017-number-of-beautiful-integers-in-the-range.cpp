bb3dd88a360847f06f6869d2a602a8b7d52e6b00
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-08-30 14:05:15 +0530

    Time: 1735 ms (22.91%), Space: 6 MB (91.36%) - LeetHub

--- 3017-number-of-beautiful-integers-in-the-range/3017-number-of-beautiful-integers-in-the-range.cpp
+++ 3017-number-of-beautiful-integers-in-the-range/3017-number-of-beautiful-integers-in-the-range.cpp
@@ -1,38 +1,24 @@
 class Solution {
 public:
     int check(int low) {
-        int even = 0;
-        int odd = 0;
-
-        while (low > 0) {
-            if (low % 2 == 0) {
-                even++;
-            } else {
-                odd++;
-            }
-
-            low /= 10;
+        int even=0,odd=0;
+        while (low>0) {
+            if (low%2==0)  ++even;
+            else  ++odd;
+            low/=10;
         }
-
-        if (even == odd) {
-            return 1;
-        } else {
-            return 0;
-        }
+        return (even==odd);
     }
-
     int numberOfBeautifulIntegers(int low, int high, int k) {
-        low = ((low - 1) / k + 1) * k;
-        int count = 0;
-        while (low <= high) {
-            count += check(low);
-            low += k;
-
-            if (low >= 100000000) {
-                return count;
-            }
+        //low = (((low - 1)/k) + 1) * k;
+        int addon=(k-(low%k))%k;
+        low=low+addon;
+        int count=0;
+        while (low<=high) {
+            count+=check(low);
+            low+=k;
+            if (low>=100000000) return count;
         }
-
         return count;
     }
 };
