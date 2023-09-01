dc3ceb5c7d6fa18b6f7fdaaca31bfa70fb54bc5c
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-09-01 23:02:21 +0530

    Time: 0 ms (100%), Space: 7.9 MB (65.8%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -5,7 +5,7 @@ public:
         std::cout.tie(nullptr);
         std::cin.tie(nullptr);
         vector<int> ans(n+1,0);
-        if(n==0) return {0};
+        if(n==0) return ans;
         ans[1]=1;
         for(int i=2;i<=n;++i){
             if(i&1) ans[i]=ans[i-1]+1;
