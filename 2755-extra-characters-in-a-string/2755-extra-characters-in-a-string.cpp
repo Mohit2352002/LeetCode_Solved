2b711af2d8c5cbefc5a2cf3eac4d4f816a3a3002
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-09-02 14:27:18 +0530

    Time: 76 ms (95.53%), Space: 97.8 MB (31.74%) - LeetHub

--- 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
@@ -40,6 +40,7 @@ public:
 
 
 
+/*
 class Solution {
 public:
     int minExtraChar(string s, vector<string> dictionary) {
@@ -58,5 +59,125 @@ public:
         }
 
         return dp[0];
+    }
+};*/
+
+
+
+
+
+
+
+
+
+/*
+class TrieNode {
+public:
+    unordered_map<char, TrieNode*> children;
+    bool is_word;
+};
+
+class Solution {
+public:
+    int minExtraChar(string s, vector<string>& dictionary) {
+        int n = s.length();
+        auto root = buildTrie(dictionary);
+        unordered_map<int, int> memo;
+
+        function<int(int)> dp = [&](int start) {
+            if (start == n) {
+                return 0;
+            }
+            if (memo.count(start)) {
+                return memo[start];
+            }
+            // To count this character as a left over character 
+            // move to index 'start + 1'
+            int ans = dp(start + 1) + 1;
+            TrieNode* node = root;
+            for (int end = start; end < n; end++) {
+                char c = s[end];
+                if (node->children.find(c) == node->children.end()) {
+                    break;
+                }
+                node = node->children[c];
+                if (node->is_word) {
+                    ans = min(ans, dp(end + 1));
+                }
+            }
+
+            return memo[start] = ans;
+        };
+
+        return dp(0);
+    }
+
+    TrieNode* buildTrie(vector<string>& dictionary) {
+        auto root = new TrieNode();
+        for (auto& word : dictionary) {
+            auto node = root;
+            for (auto& c : word) {
+                if (node->children.find(c) == node->children.end()) {
+                    node->children[c] = new TrieNode();
+                }
+                node = node->children[c];
+            }
+            node->is_word = true;
+        }
+        return root;
+    }
+};*/
+
+
+
+
+
+
+
+
+
+class TrieNode {
+public:
+    unordered_map<char, TrieNode*> children;
+    bool is_word;
+};
+
+class Solution {
+public:
+    int minExtraChar(string s, vector<string>& dictionary) {
+        int n = s.length();
+        auto root = buildTrie(dictionary);
+        vector<int> dp(n + 1, 0);
+
+        for (int start = n - 1; start >= 0; start--) {
+            dp[start] = dp[start + 1] + 1;
+            auto node = root;
+            for (int end = start; end < n; end++) {
+                if (node->children.find(s[end]) == node->children.end()) {
+                    break;
+                }
+                node = node->children[s[end]];
+                if (node->is_word) {
+                    dp[start] = min(dp[start], dp[end + 1]);
+                }
+            }
+        }
+
+        return dp[0];
+    }
+
+    TrieNode* buildTrie(vector<string>& dictionary) {
+        auto root = new TrieNode();
+        for (auto& word : dictionary) {
+            auto node = root;
+            for (auto& c : word) {
+                if (node->children.find(c) == node->children.end()) {
+                    node->children[c] = new TrieNode();
+                }
+                node = node->children[c];
+            }
+            node->is_word = true;
+        }
+        return root;
     }
 };
