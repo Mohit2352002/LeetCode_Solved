00d2eba8a251cbb374569f1c74910a6a49e94665
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-09-03 19:56:43 +0530

    Time: 35 ms (33.71%), Space: 14.8 MB (37.66%) - LeetHub

--- 1451-minimum-number-of-taps-to-open-to-water-a-garden/1451-minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1451-minimum-number-of-taps-to-open-to-water-a-garden/1451-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -1,11 +1,37 @@
+static auto _ = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+
+// class Solution {
+// public:
+//     int minTaps(int n, vector<int>& ranges) {
+//         vector<int>dp(n+1,n+2);
+//         dp[0]=0;
+//         for(int i=0;i<=n;++i){
+//             for(int j=max(0,i-ranges[i] +1) ;j<=min(n,i+ranges[i]);++j){
+//                 dp[j]=min(dp[j],dp[max(0,i-ranges[i])]+1);
+//             }
+//         }
+//         return dp[n]<(n+2)?dp[n]:-1;
+//     }
+// };
+
+
+
 class Solution {
 public:
-    int minTaps(int n, vector<int>& A) {
-        vector<int> dp(n + 1, n + 2);
-        dp[0] = 0;
-        for (int i = 0; i <= n; ++i)
-            for (int j = max(i - A[i] + 1, 0); j <= min(i + A[i], n); ++j)
-                dp[j] = min(dp[j], dp[max(0, i - A[i])] + 1);
-        return dp[n]  < n + 2 ? dp[n] : -1;
+    int minTaps(int n, vector<int>& ranges) {
+        vector<int>dp(n+1,n+2);
+        dp[0]=0;
+        for(int i=0;i<=n;++i){
+            int start=max(0,i-ranges[i]),end=min(n,i+ranges[i]);
+            for(int j=start;j<=end;++j){
+                dp[j]=min(dp[j],1+dp[max(0,i-ranges[i])]);
+            }
+        }
+        return dp[n]<(n+2)?dp[n]:-1;
     }
 };
