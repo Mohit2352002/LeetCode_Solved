e9201332621e228172ca113252ad91c752f90378
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-09-04 20:29:06 +0530

    Time: 4 ms (96.09%), Space: 14.9 MB (30.32%) - LeetHub

--- 1451-minimum-number-of-taps-to-open-to-water-a-garden/1451-minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1451-minimum-number-of-taps-to-open-to-water-a-garden/1451-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -5,6 +5,8 @@ static auto _ = [](){
     return nullptr;
 }();
 
+
+
 // class Solution {
 // public:
 //     int minTaps(int n, vector<int>& ranges) {
@@ -21,17 +23,44 @@ static auto _ = [](){
 
 
 
+
+/*
 class Solution {
 public:
     int minTaps(int n, vector<int>& ranges) {
         vector<int>dp(n+1,n+2);
         dp[0]=0;
         for(int i=0;i<=n;++i){
-            int start=max(0,i-ranges[i]),end=min(n,i+ranges[i]);
+            int start=max(0,i-ranges[i]+1),end=min(n,i+ranges[i]);
             for(int j=start;j<=end;++j){
                 dp[j]=min(dp[j],1+dp[max(0,i-ranges[i])]);
             }
         }
         return dp[n]<(n+2)?dp[n]:-1;
+    }
+};*/
+
+
+
+
+
+class Solution {
+public:
+    int minTaps(int n, vector<int>& ranges) {
+        int taps=0,far=0,curr=0;
+        vector<int>waterTill(n+1,0);
+        for(int i=0;i<=n;++i){
+            int start=max(0,i-ranges[i]),end=min(n,i+ranges[i]);
+            waterTill[start]=max(waterTill[start],end);
+        }
+        for(int i=0;i<=n;++i){
+            if(i>far) return -1;
+            if(i>curr){
+                ++taps;
+                curr=far;
+            }
+            far=max(far,waterTill[i]);
+        }
+        return taps;
     }
 };
