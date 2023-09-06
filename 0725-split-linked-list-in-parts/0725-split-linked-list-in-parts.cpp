d9c032d23b38e550de6668302f98f72b4f7c5691
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-09-06 21:40:02 +0530

    Time: 9 ms (31.1%), Space: 8.9 MB (15.59%) - LeetHub

---
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<ListNode*> splitListToParts(ListNode* head, int k) {
+        vector<ListNode*>ans(k,NULL);
+        int i=0,n=0;
+        ListNode*temp=head;
+        while(temp){
+            ++n;
+            temp=temp->next;
+        }
+        if(k>=n){
+            while(i!=n){
+                ListNode*temp=head;
+                head=head->next;
+                temp->next=NULL;
+                ans[i++]=temp;
+            }
+        }
+        else{
+            int rem=n%k,nec=n/k;
+            while(head){
+                int el=0;
+                ListNode*temp=new ListNode(-1,head);
+                ans[i++]=head;
+                while(el!=nec){
+                    temp=temp->next;
+                    head=head->next;
+                    ++el;
+                }
+                if(rem){
+                    --rem;
+                    head=head->next;
+                    temp=temp->next;
+                }
+                temp->next=NULL;
+            }
+        }
+        return ans;
+    }
+};
