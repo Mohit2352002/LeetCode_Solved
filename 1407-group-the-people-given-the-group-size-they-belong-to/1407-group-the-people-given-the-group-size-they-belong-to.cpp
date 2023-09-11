4ad7daeb318c477119e3244393d8c94f94de1118
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-09-11 08:18:39 +0530

    Time: 7 ms (87.01%), Space: 13.6 MB (28.92%) - LeetHub

---
+++ 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -0,0 +1,29 @@
+static auto _ = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+
+
+class Solution {
+public:
+    vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+        map<int,vector<int>>mp;
+        int n=groupSizes.size();
+        for(int i=0;i<n;++i) mp[groupSizes[i]].push_back(i);
+        vector<vector<int>>ans;
+        for(auto el:mp){
+            int groups=el.second.size()/el.first;
+            int i=-1;
+            while(groups--){
+                vector<int>temp(el.first);
+                for(int j=0;j<el.first;++j){
+                    temp[j]=el.second[++i];
+                }
+                ans.push_back(temp);
+            }
+        }
+        return ans;
+    }
+};
