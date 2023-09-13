530f9f42a25d868ce88a1b8a56ae24f4cd7334c6
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-09-13 09:45:51 +0530

    Time: 11 ms (94.58%), Space: 17.3 MB (82.81%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -5,6 +5,9 @@ static auto _ = [](){
     return nullptr;
 }();
 
+
+
+/*
 class Solution {
 public:
     int candy(vector<int>& ratings) {
@@ -20,5 +23,31 @@ public:
         }
         sum+=candies[n-1];
         return sum;
+    }
+};*/
+
+
+
+class Solution{
+    int sum(int &n){
+        return (n*(n+1))/2;
+    }
+public:
+    int candy(vector<int>&ratings){
+        int up=0,down=0,candies=0,n=ratings.size(),prevSlope=0;
+        for(int i=1;i<n;++i){
+            int currSlope=((ratings[i]>ratings[i-1])?1:((ratings[i]<ratings[i-1])?-1:0));
+            if((currSlope>=0 and prevSlope<0) or (currSlope==0 and prevSlope>0)){
+                candies+=(sum(up)+sum(down)+max(up,down));
+                up=0;
+                down=0;
+            }
+            if(currSlope>0) ++up;
+            else if(currSlope<0) ++down;
+            else ++candies;
+            prevSlope=currSlope;
+        }
+        candies+=(sum(up)+sum(down)+max(up,down)+1);
+        return candies;
     }
 };
