49c21f143c24c322d8d7d810b7bef89087e343d8
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-09-13 07:55:00 +0530

    Time: 10 ms (96.96%), Space: 17.8 MB (31.94%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -14,7 +14,8 @@ public:
             if(ratings[i]>ratings[i-1]) candies[i]=candies[i-1]+1;
         }
         for(int i=n-2;i>=0;--i){
-            if(ratings[i]>ratings[i+1] and candies[i]<=candies[i+1]) candies[i]=candies[i+1]+1;
+            if(ratings[i]>ratings[i+1] and candies[i]<=candies[i+1]) 
+                candies[i]=candies[i+1]+1;
             sum+=candies[i];
         }
         sum+=candies[n-1];
