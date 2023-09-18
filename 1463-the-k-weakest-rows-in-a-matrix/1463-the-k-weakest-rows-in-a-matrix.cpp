4b4482dca169374cee7036812eb1661431676e8a
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-09-18 08:15:35 +0530

    Time: 7 ms (82.57%), Space: 10.8 MB (13.97%) - LeetHub

--- 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.cpp
+++ 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.cpp
@@ -5,6 +5,8 @@ static auto _ = [](){
     return nullptr;
 }();
 
+
+/*
 #define pii pair<int,int>
 class Solution {
 public:
@@ -24,6 +26,53 @@ public:
         while(k--){
             ans[k]=pq.top().second;
             pq.pop();
+        }
+        return ans;
+    }
+};
+*/
+
+
+
+
+
+// CompareHeapElements is used for modifying the comparator of priority queue, as per question
+// struct CompareHeapElements {
+//     bool operator()(pair<int,int> const& p1, pair<int,int> const& p2)
+//     {
+//         if(p1.first<p2.first){
+//             return true;
+//         }
+//         else if(p1.first==p2.first && p1.second<p2.second){
+//             return true;
+//         }
+//         return false;
+//     }
+// };
+
+class Solution {
+    int calculateSoldierCount(vector<int>&v) {
+        int l=0, h=v.size()-1;
+        while(l<=h) {
+            int mid=l+((h-l)>>1);
+            if(v[mid]==0) h=mid-1;
+            else l=mid+1;
+        }
+        return l;
+    }
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        priority_queue<pair<int,int>> max_heap;
+        for(int i=0;i<mat.size();++i){
+            max_heap.push({calculateSoldierCount(mat[i]),i});
+            if(max_heap.size()>k){
+                max_heap.pop();
+            }
+        }
+        vector<int>ans(k);
+        while(max_heap.size()){
+            ans[--k]=max_heap.top().second;
+            max_heap.pop();
         }
         return ans;
     }
