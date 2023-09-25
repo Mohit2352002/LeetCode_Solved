d435884423eae0c93e078c26a4eb7e3f937ed2af
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-09-25 05:53:15 +0530

    Time: 4 ms (48.11%), Space: 6.9 MB (16.46%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int n=t.size();
+        if(n==1) return t[0];
+        int freq[26]={};
+        for(int i=0;i<n;++i){
+            ++freq[t[i]-'a'];
+            if(i!=(n-1)) --freq[s[i]-'a'];
+        }
+        for(int i=0;i<26;++i) if(freq[i]>0) return 'a'+i;
+        return 'a';
+    }
+};
