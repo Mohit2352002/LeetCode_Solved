b16008beaa407548ef892153587042be26dfc6fa
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-10-03 12:32:45 +0530

    Time: 0 ms (100%), Space: 7.7 MB (7.1%) - LeetHub

---
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        int n=nums.size(),ans=0;
+        for(int i=0;i<n;++i){
+            ans+=mp[nums[i]];
+            ++mp[nums[i]];
+        }
+        return ans;
+    }
+};
