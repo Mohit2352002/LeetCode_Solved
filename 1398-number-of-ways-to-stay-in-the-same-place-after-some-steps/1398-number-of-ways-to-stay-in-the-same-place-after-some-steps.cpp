d74b4bc91442b2905388ee1be0f4b008f2dfa5c9
Mohit2352002 <80191777+Mohit2352002@users.noreply.github.com>
2023-10-15 12:29:08 +0530

    Time: 9 ms (89.18%), Space: 6.6 MB (93.94%) - LeetHub

--- 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/1398-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
+++ 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/1398-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -52,6 +52,7 @@ public:
 //space optimised dp
 class Solution {
 public:
+/*
     int numWays(int steps, int arrLen){
         int MOD=1e9+7;
         arrLen=min(steps/2 +1,arrLen);
@@ -64,5 +65,16 @@ public:
             prevdp=dp;
         }
         return dp[0];
+    }*/
+    int numWays(int steps, int arrLen) {
+        int sz = min(steps / 2 + 1, arrLen);
+        vector<int> v1(sz + 2), v2(sz + 2);
+        v1[1] = 1;
+        while (steps-- > 0) {
+            for (auto i = 1; i <= sz; ++i)
+                v2[i] = ((long)v1[i] + v1[i - 1] + v1[i + 1]) % 1000000007;
+            swap(v1, v2);
+        }
+        return v1[1];
     }
 };
